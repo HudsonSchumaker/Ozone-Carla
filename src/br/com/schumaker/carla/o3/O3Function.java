57a5bc936fe04c13a0e6e0ec7ae2e754a6f537bb
Schumaker <hudson@schumaker.com.br>
2020-08-05 00:05:23 +0200

    Added function internal name to O3Function class.

--- src/br/com/schumaker/carla/o3/O3Function.java
+++ src/br/com/schumaker/carla/o3/O3Function.java
@@ -4,21 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
- * This class represents the functions of O³ pl.
- * Ex:
- * f: main() {
- *      ; some code here...
- * }
- * 
+ * This class represents the functions of O³ pl. Ex: f: main() { ; some code
+ * here... }
+ *
  * @author schumaker
  */
-
 @Data
 @AllArgsConstructor
 public class O3Function {
-    
+
     private String name;
+    private String internalName;
     private boolean isMain;
     //private List<O3Variable> params; 
     private IO3Statement statement;
+
+    @Override
+    public String toString() {
+        return "{\n"
+                + "Name : " + name + ",\n"
+                + "Internal Name : " + internalName + ",\n"
+                + "Is Main : " + isMain + ",\n"
+                + "Variables : " + ((O3FunctionStatement)statement).getVariables()
+                + "\n}";
+    }
 }
