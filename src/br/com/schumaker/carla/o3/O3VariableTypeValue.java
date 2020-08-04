c3ba255c3d5c5450637186ff761eb7c5803bb55b
Schumaker <hudson@schumaker.com.br>
2020-08-05 00:06:15 +0200

    Java Doc.

--- src/br/com/schumaker/carla/o3/O3VariableTypeValue.java
+++ src/br/com/schumaker/carla/o3/O3VariableTypeValue.java
@@ -15,8 +15,16 @@ public class O3VariableTypeValue<O3VariableType, V> {
         this.value = v;
     }
     
-    public static <O3VariableType, V> O3VariableTypeValue<O3VariableType, V> of(O3VariableType left, V right) {
-        return new O3VariableTypeValue<>(left, right);
+    /**
+     * 
+     * @param <O3VariableType>
+     * @param <V> Type of the value 
+     * @param type
+     * @param value
+     * @return a new instance of variable type and value.
+     */
+    public static <O3VariableType, V> O3VariableTypeValue<O3VariableType, V> of(O3VariableType type, V value) {
+        return new O3VariableTypeValue<>(type, value);
     }
 
     public O3VariableType getType() {
