15fe1414da6b349bd9749d7fcb0373375de2c76d
Schumaker <hudson@schumaker.com.br>
2020-08-04 21:49:55 +0200

    More unit tests for LexerHelper class.

--- test/br/com/schumaker/carla/lexer/LexerHelperTest.java
+++ test/br/com/schumaker/carla/lexer/LexerHelperTest.java
@@ -1,5 +1,6 @@
 package br.com.schumaker.carla.lexer;
 
+import br.com.schumaker.carla.test.O3TestHelper;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -21,4 +22,51 @@ public class LexerHelperTest {
         assertTrue(result);
     }
     
+    @Test
+    public void testIsConditionalStatement() {
+        // Preparation
+        var data = "if (1 > x) {";
+       
+        // Test
+        var result = LexerHelper.isConditionalStatement(data);
+        
+        // Assertion
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsLoopStatement() {
+        // Preparation
+        var data = "while( x > 10) {";
+       
+        // Test
+        var result = LexerHelper.isLoopStatement(data);
+        
+        // Assertion
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsVariableDeclaration() {
+        // Preparation
+        var data = "v: number = 56";
+       
+        // Test
+        var result = LexerHelper.isVariableDeclaration(data);
+        
+        // Assertion
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testContainsFunctionMain() throws Exception {
+        // Preparation
+        var file  = O3TestHelper.createO3File();
+       
+        // Test
+        var result = LexerHelper.containsFunctionMain(file);
+        
+        // Assertion
+        assertTrue(result);
+    }
 }
