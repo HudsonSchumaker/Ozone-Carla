80ffe9a019e2dda001ab36810df10dfce58089e2
Schumaker <hudson@schumaker.com.br>
2020-08-05 22:46:13 +0200

    Add new method on O3FileBuilder class.

--- src/br/com/schumaker/carla/files/O3FileBuilder.java
+++ src/br/com/schumaker/carla/files/O3FileBuilder.java
@@ -7,36 +7,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class builds O3Files 
+ * This class builds O3Files
  * @see O3File
- * 
  * @author schumaker
  */
 public class O3FileBuilder implements FileBuilder<O3File> {
-    
+
     @Override
     public O3File build(String path) throws Exception {
         var o3FileReader = new O3FileReader();
         var lines = o3FileReader.read(path);
-        
+
         var file = new O3File(
-                FileUtils.getName(path), 
-                FileUtils.getClearPath(path), 
+                FileUtils.getName(path),
+                FileUtils.getClearPath(path),
                 this.createLines(lines));
-        
+
         if (!LexerHelper.containsFunctionMain(file)) {
             throw new FunctionMainNotFoundException();
         }
-        
+
         this.setFunctionHeaders(file);
         this.setConditionalStatements(file);
         this.setLoopStatements(file);
         this.setVariableDeclarations(file);
+        this.setFunctionCalls(file);
         return file;
     }
-    
+
     /**
      * Creates the O3FileLine from the rawLines read form a .o3 source file.
+     *
      * @param rawLines
      * @return A list of O3FileLine
      */
@@ -47,10 +48,11 @@ public class O3FileBuilder implements FileBuilder<O3File> {
         }
         return lines;
     }
-    
+
     /**
      * Sets the O3FileLines that have a function header.
-     * @param file 
+     *
+     * @param file
      */
     private void setFunctionHeaders(O3File file) {
         for (O3FileLine line : file.getLines()) {
@@ -59,31 +61,47 @@ public class O3FileBuilder implements FileBuilder<O3File> {
             }
         }
     }
-    
+
     private void setConditionalStatements(O3File file) {
         for (O3FileLine line : file.getLines()) {
-            if (LexerHelper.isConditionalStatement(line.getData())) {
-                line.setConditionalStatement(true);
+            if (!line.isFunctionHeader()) {
+                if (LexerHelper.isConditionalStatement(line.getData())) {
+                    line.setConditionalStatement(true);
+                }
             }
         }
     }
-    
+
     private void setLoopStatements(O3File file) {
         for (O3FileLine line : file.getLines()) {
-            if (LexerHelper.isLoopStatement(line.getData())) {
-                line.setLoopStatement(true);
+            if (!line.isFunctionHeader()) {
+                if (LexerHelper.isLoopStatement(line.getData())) {
+                    line.setLoopStatement(true);
+                }
             }
         }
     }
-    
+
     private void setVariableDeclarations(O3File file) {
         for (O3FileLine line : file.getLines()) {
-            if (LexerHelper.isVariableDeclaration(line.getData())) {
-                line.setVariableDeclaration(true);
+            if (!line.isFunctionHeader()) {
+                if (LexerHelper.isVariableDeclaration(line.getData())) {
+                    line.setVariableDeclaration(true);
+                }
             }
         }
     }
-    
+
+    private void setFunctionCalls(O3File file) {
+        for (O3FileLine line : file.getLines()) {
+            if (!line.isFunctionHeader()) {
+                if (LexerHelper.isAnExpression(line.getData())) {
+                    line.setFunctionCall(true);
+                }
+            }
+        }
+    }
+
     private void setReturnStatements(O3File file) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
