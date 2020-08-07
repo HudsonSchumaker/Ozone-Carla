fc8c85b938e9d943d50013c1f0c6220f8e0d0e30
Schumaker <hudson@schumaker.com.br>
2020-08-07 06:01:39 +0200

    Add built in functions.

---
+++ src/br/com/schumaker/carla/o3/O3FunctionTable.java
@@ -0,0 +1,16 @@
+package br.com.schumaker.carla.o3;
+
+/**
+ *
+ * @author schumaker
+ */
+public class O3FunctionTable {
+    
+    public static final String PRINT = "print";
+    public static final String POW   = "pow";
+    public static final String SQRT  = "sqrt";
+    public static final String ABS   = "abs";
+    public static final String MIN   = "min";
+    public static final String MAX   = "max";
+    
+}
