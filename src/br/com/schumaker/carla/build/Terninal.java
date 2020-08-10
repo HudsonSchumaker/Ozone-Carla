07bd84bd7fee79c44710938d5c93af3445976e27
Schumaker <hudson@schumaker.com.br>
2020-08-10 21:21:37 +0200

    Terminal for compilation

---
+++ src/br/com/schumaker/carla/build/Terninal.java
@@ -0,0 +1,16 @@
+package br.com.schumaker.carla.build;
+
+/**
+ *
+ * @author schumaker
+ */
+public class Terninal implements Zsh {
+
+    @Override
+    public void executeCommnad(String command) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
