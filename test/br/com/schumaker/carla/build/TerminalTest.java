77225af099cddb58d8d953b7eb507d95df80c7c5
Schumaker <hudson@schumaker.com.br>
2020-08-10 21:48:36 +0200

    Terminal test.

---
+++ test/br/com/schumaker/carla/build/TerminalTest.java
@@ -0,0 +1,28 @@
+package br.com.schumaker.carla.build;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author schumaker
+ */
+public class TerminalTest {
+    
+    @Test
+    public void testExecuteCommand() throws InterruptedException {
+        // Prepariton
+        var path = System.getProperty("user.dir");
+        var cmd = "pwd";
+        
+        // Test
+        var tested = new Terninal();
+        tested.executeCommnad(cmd);
+        
+        // Assertion
+        Thread.sleep(500); // wait for clean the buffer. 
+        assertEquals(path, tested.getReturns().get(0));
+        
+        
+    }
+}
