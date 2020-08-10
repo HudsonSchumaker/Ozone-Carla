863057a7a5911fe85794778f9832f7454a9c1172
Schumaker <hudson@schumaker.com.br>
2020-08-10 22:14:17 +0200

    Move to another package.

---
+++ src/br/com/schumaker/carla/nasm/NasmCommon.java
@@ -0,0 +1,12 @@
+package br.com.schumaker.carla.nasm;
+
+/**
+ *
+ * @author schumaker
+ */
+public final class NasmCommon {
+    private NasmCommon() {}
+    
+    public static final String OFFSET = "0x2000000";
+    
+}
