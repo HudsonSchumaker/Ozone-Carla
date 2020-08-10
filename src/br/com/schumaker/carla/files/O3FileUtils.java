05d5c043ec654e0d5d8e2ac4e3d0ed380094340b
Schumaker <hudson@schumaker.com.br>
2020-08-10 22:23:50 +0200

    Renamed for avoid names conflicts.

---
+++ src/br/com/schumaker/carla/files/O3FileUtils.java
@@ -0,0 +1,44 @@
+package br.com.schumaker.carla.files;
+
+import java.io.File;
+
+/**
+ *
+ * @author schumaker
+ */
+public final class O3FileUtils {
+    
+    private O3FileUtils() {}
+    
+    public static String getFileExtension(File file) {
+        return getFileExtension(file.getAbsolutePath());
+    }
+
+    public static String getFileExtension(String fileName) {
+        String[] ext = fileName.split("\\.");
+        int size = ext.length;
+        String result = "";
+        if (size > 1) {
+            result = ext[size - 1];
+        }
+        return result.toLowerCase();
+    }
+
+    public static String getClearName(File file) {
+        return file.getName().split("\\.")[0];
+    }
+
+    public static String getClearName(String path) {
+        return getClearName(new File(path));
+    }
+
+    public static String getName(String path) {
+        File file = new File(path);
+        return file.getName();
+    }
+    
+    public static String getClearPath(String path) {
+        int pos = path.lastIndexOf("/");
+        return path.substring(0, pos + 1);
+    }
+}
