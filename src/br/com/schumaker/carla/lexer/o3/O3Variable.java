81a2feb7b9a9eefc31a5a04d348a898d02026cd8
Schumaker <hudson@schumaker.com.br>
2020-08-13 00:11:49 +0200

    Small change on O3Variable class.

--- src/br/com/schumaker/carla/lexer/o3/O3Variable.java
+++ src/br/com/schumaker/carla/lexer/o3/O3Variable.java
@@ -4,28 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
- * This class represents the variables of O³ pl.
- * Ex:
- * v: pi = 3.14f
- * 
+ * This class represents the variables of O³ pl. Ex: v: pi = 3.14f
+ *
  * @author schumaker
  */
-
 @Data
 @AllArgsConstructor
-public class O3Variable {
-    
+public class O3Variable implements IO3Variable {
+
     private String name;
     private String internalName;
-    private O3VariableTypeValue<O3VariableType, ?> typeValue;       
-    
-   @Override
-   public String toString() {
-       return "{\n"
-               + "\tName : " + name + ",\n"
-               + "\tInternal Name : " + internalName + ",\n"
-               + "\tType : " + typeValue.getType().getName() + ",\n"
-               + "\tValue: " + typeValue.getValue()
-               + "\n}";
-   }
+    private O3VariableTypeValue<O3VariableType, ?> typeValue;
+
+    @Override
+    public O3VariableType getType() {
+        return typeValue.getType();
+    }
+
+    @Override
+    public String toString() {
+        return "{\n"
+                + "\tName : " + name + ",\n"
+                + "\tInternal Name : " + internalName + ",\n"
+                + "\tType : " + typeValue.getType().getName() + ",\n"
+                + "\tValue: " + typeValue.getValue()
+                + "\n}";
+    }
 }
