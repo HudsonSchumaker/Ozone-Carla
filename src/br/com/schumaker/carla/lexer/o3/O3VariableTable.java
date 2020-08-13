202cff7d94af544ac97817302d5382f827aa5930
Schumaker <hudson@schumaker.com.br>
2020-08-13 22:34:30 +0200

    Added O3VariableTable class.

---
+++ src/br/com/schumaker/carla/lexer/o3/O3VariableTable.java
@@ -0,0 +1,16 @@
+package br.com.schumaker.carla.lexer.o3;
+
+import br.com.schumaker.carla.files.O3FileLine;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author schumaker
+ */
+@Data
+@AllArgsConstructor
+public class O3VariableTable implements IO3VariableTable {
+   private List<O3FileLine> lines;
+}
