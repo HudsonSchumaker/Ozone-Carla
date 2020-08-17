e9142ca1e0763d3a3f143054dbf34b2a0f1a8e7c
Schumaker <hudson@schumaker.com.br>
2020-08-17 23:17:01 +0200

    Added O3Library class.

---
+++ src/br/com/schumaker/carla/o3/O3Library.java
@@ -0,0 +1,14 @@
+package br.com.schumaker.carla.o3;
+
+/**
+ *
+ * @author schumaker
+ */
+public class O3Library {
+    public static final String STRINGS_O = "O3String.o";
+    public static final String STRINGS_C = "O3String.c";
+    
+    public static final String PRINT_O = "O3Print.o";
+    public static final String PRINT_C = "O3Print.c";  
+}
+ 
