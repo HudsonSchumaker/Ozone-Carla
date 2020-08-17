fcd7e24572a5be4ae26635e5b0a728b35d2da2d3
Schumaker <hudson@schumaker.com.br>
2020-08-17 23:00:28 +0200

    Added x64Library class.

---
+++ src/br/com/schumaker/metalx64/x64Library.java
@@ -0,0 +1,15 @@
+package br.com.schumaker.metalx64;
+
+import java.util.List;
+
+/**
+ *
+ * @author schumaker
+ */
+
+public class x64Library {
+   
+    private List<x64Function> x64Functions;
+    
+    
+}
