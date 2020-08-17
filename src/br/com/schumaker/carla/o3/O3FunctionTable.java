e479f35146a2ea6edee18f3d04dbfbca075074d5
Schumaker <hudson@schumaker.com.br>
2020-08-17 23:51:18 +0200

    Updated O3FunctionTable class.

--- src/br/com/schumaker/carla/o3/O3FunctionTable.java
+++ src/br/com/schumaker/carla/o3/O3FunctionTable.java
@@ -1,18 +1,29 @@
 package br.com.schumaker.carla.o3;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author schumaker
  */
 public class O3FunctionTable {
     
-    // Strings
-    public static final String PRINT = "print";
-    public static final String UPPER_CASE = "upperCase";
-    public static final String LOWER_CASE = "lowerCase";
-    public static final String CAMEL_CASE = "camelCase";
+     // Print uses PRT as indentifier
+    public static final String PRT_PRINT = "print";
+    public static final String PRT_PRINT_LN = "printLn";
     
-    
+    // Strings uses STR as indentifier
+    public static final String STR_UPPER_CASE = "upperCase";
+    public static final String STR_LOWER_CASE = "lowerCase";
+    public static final String STR_CAMEL_CASE = "camelCase";
+    public static final String STR_PASCAL_CASE = "pascalCase";
+    public static final String STR_SNAKE_CASE = "snakeCase";
+    public static final String STR_KEBAB_CASE = "kebabCase";
+        
     // Math
     public static final String POW   = "pow";
     public static final String SQRT  = "sqrt";
@@ -20,5 +31,63 @@ public class O3FunctionTable {
     public static final String MIN   = "min";
     public static final String MAX   = "max";
     
+    private List<String> allFunction = new ArrayList<>();
+    private Map<String, String> strings = new HashMap<>();
+    private Map<String, String> print = new HashMap<>();
+
+    public O3FunctionTable() throws Exception {
+        this.loadValues();
+        this.loadStringLib();
+        this.loadPrintLib();
+    }
+            
+    public boolean functionExists(String name) {
+        return allFunction.contains(name);
+    }
     
+    private void loadValues() throws Exception {
+        Class<?> thisClass = null;
+        try {
+            thisClass = Class.forName(this.getClass().getName());
+
+            Field[] aClassFields = thisClass.getDeclaredFields();
+            for (Field f : aClassFields) {
+                allFunction.add(f.getName());
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    private void loadStringLib() throws Exception {
+        Class<?> thisClass = null;
+        try {
+            thisClass = Class.forName(this.getClass().getName());
+
+            Field[] aClassFields = thisClass.getDeclaredFields();
+            for (Field f : aClassFields) {
+                if (f.getName().startsWith("STR_")) {
+                    strings.put((String)f.get(this), O3Library.STRINGS_O);
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    private void loadPrintLib() throws Exception {
+        Class<?> thisClass = null;
+        try {
+            thisClass = Class.forName(this.getClass().getName());
+
+            Field[] aClassFields = thisClass.getDeclaredFields();
+            for (Field f : aClassFields) {
+                if (f.getName().startsWith("PRT_")) {
+                    print.put((String)f.get(this), O3Library.PRINT_O);
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
