235cba01d8ab2b9ef283cf70d73f561d742e4245
Schumaker <hudson@schumaker.com.br>
2020-08-18 19:18:50 +0200

    Added testGetLibNameByFunctionName test.

--- test/br/com/schumaker/carla/o3/O3FunctionTableTest.java
+++ test/br/com/schumaker/carla/o3/O3FunctionTableTest.java
@@ -1,5 +1,6 @@
 package br.com.schumaker.carla.o3;
 
+import org.junit.Assert;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
@@ -18,9 +19,20 @@ public class O3FunctionTableTest {
         var camelCase = "camelCase";
         var draw = "draw";
         
-        // Test - Assertion
+        // Test - Assertions
         assertTrue(tested.functionExists(print));
         assertTrue(tested.functionExists(camelCase));
         assertFalse(tested.functionExists(draw));
+    }
+    
+    @Test
+    public void testGetLibNameByFunctionName() throws Exception {
+        // Preparation
+        var tested = new O3FunctionTable();
+        
+        // Test - Assertions
+        Assert.assertEquals(O3Library.PRINT_O, tested.getLibNameByFunctionName(O3FunctionTable.PRT_PRINT));
+        Assert.assertEquals(O3Library.STRINGS_O, tested.getLibNameByFunctionName(O3FunctionTable.STR_CAMEL_CASE));
+        Assert.assertEquals("", tested.getLibNameByFunctionName("draw"));
     }
 }
