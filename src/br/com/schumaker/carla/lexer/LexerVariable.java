1efafdfd6f39a42e92e37e3910cf4c633df35e71
Schumaker <hudson@schumaker.com.br>
2020-08-21 00:05:49 +0200

    Fixes in LexeVariable class.

--- src/br/com/schumaker/carla/lexer/LexerVariable.java
+++ src/br/com/schumaker/carla/lexer/LexerVariable.java
@@ -1,7 +1,7 @@
- package br.com.schumaker.carla.lexer;
+package br.com.schumaker.carla.lexer;
 
 import br.com.schumaker.carla.files.O3FileLine;
-import br.com.schumaker.carla.o3.O3Keyword;
+import br.com.schumaker.carla.o3.O3CoreKeyword;
 import br.com.schumaker.carla.lexer.o3.O3Variable;
 import br.com.schumaker.carla.lexer.o3.O3VariableType;
 import br.com.schumaker.carla.lexer.o3.O3VariableTypeValue;
@@ -11,6 +11,7 @@ import java.util.List;
 
 /**
  * This class create the O³ variables representations.
+ *
  * @author schumaker
  */
 public class LexerVariable {
@@ -23,64 +24,71 @@ public class LexerVariable {
         return variables;
     }
 
+    /**
+     * Get a declared variable inside a function.
+     * @param functionName
+     * @param line
+     * @return O3Variable with type and value.
+     */
     public O3Variable getVariable(String functionName, O3FileLine line) {
         var type = this.getType(line.getData());
         switch (type) {
             case STRING:
                 return new O3Variable(this.getVariableName(line.getData()),
-                        this.getVariableInternalName(functionName,line.getData()),
+                        this.getVariableInternalName(functionName, line.getData()),
                         O3VariableTypeValue.of(type, this.getValueString(line.getData())));
             case BOOL:
                 return new O3Variable(this.getVariableName(line.getData()),
-                        this.getVariableInternalName(functionName,line.getData()),
+                        this.getVariableInternalName(functionName, line.getData()),
                         O3VariableTypeValue.of(type, this.getValueBoolean(line.getData())));
             case FLOAT:
                 return new O3Variable(this.getVariableName(line.getData()),
-                        this.getVariableInternalName(functionName,line.getData()),
+                        this.getVariableInternalName(functionName, line.getData()),
                         O3VariableTypeValue.of(type, this.getValueFloat(line.getData())));
+            
+            case DOUBLE:
+                return new O3Variable(this.getVariableName(line.getData()),
+                        this.getVariableInternalName(functionName, line.getData()),
+                        O3VariableTypeValue.of(type, this.getValueDouble(line.getData())));
+                
             default:
                 return new O3Variable(this.getVariableName(line.getData()),
-                        this.getVariableInternalName(functionName,line.getData()),
-                        O3VariableTypeValue.of(type, this.getValueInteger(line.getData())));  
+                        this.getVariableInternalName(functionName, line.getData()),
+                        O3VariableTypeValue.of(type, this.getValueInteger(line.getData())));
         }
     }
-    
+
     /**
-     * Creates the the parameters list of a function, based on the name of the 
-     * function and the declared parameters.
-     * Formula: p_ + functionName +_ + nameOfVarible + :
-     * Ex:
-     * f: print(v: text) {
-     *   ;some code...
-     * }
-     * The internal name of the "v: text" variable will be "p_main_text:" 
-     * 
+     * Creates the the parameters list of a function, based on the name of the
+     * function and the declared parameters. Formula: p_ + functionName +_ +
+     * nameOfVarible + : Ex: f: print(v: text) { ;some code... } The internal
+     * name of the "v: text" variable will be "p_main_text:"
+     *
      * @param functionName name of the function that the parameter is declared
      * @param headerLine O3Line that holds the function signature
      * @return the parameters list of the function
      */
     public List<O3Variable> getParameters(String functionName, O3FileLine headerLine) {
-        var raw = headerLine.getData().trim().substring(
-                headerLine.getData().trim().indexOf(O3Keyword.OPEN_EXPRESSION) + 1,
-                headerLine.getData().trim().indexOf(O3Keyword.CLOSE_EXPRESSION));
-        
+        var raw = headerLine.getData().trim().substring(headerLine.getData().trim().indexOf(O3CoreKeyword.OPEN_EXPRESSION) + 1,
+                headerLine.getData().trim().indexOf(O3CoreKeyword.CLOSE_EXPRESSION));
+
         var rawParams = raw.split(",");
         if (!this.validParamsArray(rawParams)) {
             return new ArrayList<>();
         }
-        
+
         var params = new ArrayList<O3Variable>();
         for (String p : rawParams) {
             var clean = p.trim();
-            var param = clean.substring(O3Keyword.VARIABLE.length(), clean.length()).trim();
+            var param = clean.substring(O3CoreKeyword.VARIABLE.length(), clean.length()).trim();
             var internalName = "p_" + functionName + "_" + param + ":";
-            params.add(new O3Variable(param, internalName, 
-                    O3VariableTypeValue.of(O3VariableType.PARAM, 
+            params.add(new O3Variable(param, internalName,
+                    O3VariableTypeValue.of(O3VariableType.PARAM,
                             O3VariableType.PARAM.getDefaultValue())));
-        }        
+        }
         return params;
     }
-    
+
     public boolean validParamsArray(String[] rawParams) {
         if (rawParams.length == 0) {
             return false;
@@ -94,62 +102,77 @@ public class LexerVariable {
         return true;
     }
 
+    /**
+     * Resolves the type of declared variable with assignment.
+     *
+     * @param data Raw like from .o3 file source
+     * @return
+     */
     public O3VariableType getType(String data) {
-        var value = data.substring(data.indexOf(O3Keyword.ASSINGN), data.length()).trim();
-         
-        if (value.contains("\"")){
+        var value = data.substring(data.indexOf(O3CoreKeyword.ASSINGN), data.length()).trim();
+
+         if (value.contains("\"")) {
             return O3VariableType.STRING;
         } else {
-           if (value.contains(O3Keyword.TRUE) || value.contains(O3Keyword.FALSE)) {
-               return O3VariableType.BOOL;
-           } 
-           if (value.contains(O3Keyword.FLOATING_POINT_SIGN)) {
-               return O3VariableType.FLOAT;
-           }
-           return O3VariableType.INT; 
+            if (value.contains(O3CoreKeyword.TRUE) || value.contains(O3CoreKeyword.FALSE)) {
+                return O3VariableType.BOOL;
+            }
+
+            if (value.contains(O3CoreKeyword.FLOATING_POINT_SIGN)) {
+                if (data.contains(O3CoreKeyword.FLOAT_SIGN)) {
+                    return O3VariableType.FLOAT;
+                }
+
+                if (data.contains(O3CoreKeyword.DOUBLE_SIGN)) {
+                    return O3VariableType.DOUBLE;
+                }
+            }
+            return O3VariableType.INT;
         }
     }
 
     public String getValueString(String data) {
         var clean = data.trim();
-        return clean.substring(clean.indexOf(O3Keyword.ASSINGN) + 1, clean.length()).trim();
+        return clean.substring(clean.indexOf(O3CoreKeyword.ASSINGN) + 1, clean.length()).trim();
     }
-    
+
     public Boolean getValueBoolean(String data) {
         var clean = data.trim();
-        var value = clean.substring(clean.indexOf(O3Keyword.ASSINGN) + 1, clean.length()).trim();
+        var value = clean.substring(clean.indexOf(O3CoreKeyword.ASSINGN) + 1, clean.length()).trim();
         return Boolean.valueOf(value);
     }
-    
+
     public Integer getValueInteger(String data) {
         var clean = data.trim();
-        var value = clean.substring(clean.indexOf(O3Keyword.ASSINGN) + 1, clean.length()).trim();
+        var value = clean.substring(clean.indexOf(O3CoreKeyword.ASSINGN) + 1, clean.length()).trim();
         return Integer.valueOf(value);
     }
-    
+
     public Float getValueFloat(String data) {
         var clean = data.trim();
-        var value = clean.substring(clean.indexOf(O3Keyword.ASSINGN) + 1, clean.length() - 1).trim();
+        var value = clean.substring(clean.indexOf(O3CoreKeyword.ASSINGN) + 1, clean.length() - 1).trim();
         return Float.valueOf(value);
     }
     
+    public Double getValueDouble(String data) {
+        var clean = data.trim();
+        var value = clean.substring(clean.indexOf(O3CoreKeyword.ASSINGN) + 1, clean.length() - 1).trim();
+        return Double.valueOf(value);
+    }
+
     public String getVariableName(String data) {
         var clean = data.trim();
-        var name = clean.substring(O3Keyword.VARIABLE.length(), clean.length()).trim();
-        name = name.substring(0, name.indexOf(O3Keyword.ASSINGN)).trim();
+        var name = clean.substring(O3CoreKeyword.VARIABLE.length(), clean.length()).trim();
+        name = name.substring(0, name.indexOf(O3CoreKeyword.ASSINGN)).trim();
         return name;
     }
-    
+
     /**
-     * Creates the the internal name of a variable, based on the name of the 
-     * function and the declared name of the variable.
-     * Formula: functionName + _ + nameOfVarible + :
-     * Ex:
-     * f: main() {
-     *   v: text = "Hello World!!!"
-     * }
-     * The internal name of the "v: text" variable will be "main_text:" 
-     * 
+     * Creates the the internal name of a variable, based on the name of the
+     * function and the declared name of the variable. Formula: functionName + _
+     * + nameOfVarible + : Ex: f: main() { v: text = "Hello World!!!" } The
+     * internal name of the "v: text" variable will be "main_text:"
+     *
      * @param functionName name of the function that the variable is inside
      * @param data content from a O3FileLine.getData()
      * @return variable internal name
