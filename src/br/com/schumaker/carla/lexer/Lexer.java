608403926f8065959304ca6e8be368c693e451e6
Schumaker <hudson@schumaker.com.br>
2020-08-22 17:15:01 +0200

    Changes on Lexer class.

--- src/br/com/schumaker/carla/lexer/Lexer.java
+++ src/br/com/schumaker/carla/lexer/Lexer.java
@@ -2,16 +2,38 @@ package br.com.schumaker.carla.lexer;
 
 import br.com.schumaker.carla.files.O3File;
 import br.com.schumaker.carla.lexer.o3.O3Atom;
+import br.com.schumaker.carla.lexer.o3.O3AtomVariableTable;
+import br.com.schumaker.carla.lexer.o3.O3Function;
+import br.com.schumaker.carla.lexer.o3.O3FunctionVariableTable;
+import java.util.List;
 
 /**
  * Main class of O3 (Carla Compiler) Lexer.
+ *
  * @author schumaker
  */
-public class Lexer {
-    
+public class Lexer implements ILexer {
+
+    @Override
     public O3Atom createProgram(O3File o3File) {
         var lexerFunction = new LexerFunction();
         var functions = lexerFunction.getFunctions(o3File);
-        return new O3Atom(o3File.getName(),lexerFunction.getO3FunctionTable() , functions);
-    } 
+
+        return new O3Atom(o3File.getName(), 
+                this.fillAtomVariableTable(functions),
+                lexerFunction.getO3FunctionTable(), 
+                functions);
+    }
+
+    @Override
+    public O3AtomVariableTable fillAtomVariableTable(List<O3Function> functions) {
+        var atomTable = new O3AtomVariableTable();
+        for (var func : functions) {
+            var table = (O3FunctionVariableTable)func.getVariableTable();
+            for (var v03var : table.getFuncVars()) {
+              atomTable.add(v03var);
+            }
+        }
+        return atomTable;
+    }
 }
