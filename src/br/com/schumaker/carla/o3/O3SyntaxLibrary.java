c5444efce7a990caacbba7184f31940b5cc5b48a
Schumaker <hudson@schumaker.com.br>
2020-08-23 00:15:15 +0200

    Change name class.

---
+++ src/br/com/schumaker/carla/o3/O3SyntaxLibrary.java
@@ -0,0 +1,18 @@
+package br.com.schumaker.carla.o3;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public class O3SyntaxLibrary {
+    
+    public static final List<String> LIB_ID = Arrays.asList("STR_", "PRT_", "MATH_");
+    public static final String STRINGS_O = "O3String.o";
+    public static final String STRINGS_C = "O3String.c";
+    
+    public static final String PRINT_O = "O3Print.o";
+    public static final String PRINT_C = "O3Print.c";    
+}
