a83e79f6b7c74ae8c94dee7947e009c5eb01f324
Schumaker <hudson@schumaker.com.br>
2020-08-22 19:44:17 +0200

    Added X64O3VariableMap class.

---
+++ src/br/com/schumaker/halogenx64/X64O3VariableMap.java
@@ -0,0 +1,29 @@
+  package br.com.schumaker.halogenx64;
+
+/**
+ *
+ * @author schumaker
+ * @param <A>
+ */
+public class X64O3VariableMap<N, A> {
+    
+    private final N internalName;
+    private final A asmType;
+
+    private X64O3VariableMap(N n, A a) {
+        this.internalName = n;
+        this.asmType = a;
+    }
+
+    public static <N, T> X64O3VariableMap<N, T> of(N internalName, T type) {
+        return new X64O3VariableMap<>(internalName, type);
+    }
+
+    public N getInternalName() {
+        return internalName;
+    }
+
+    public A getAsmType() {
+        return asmType;
+    }
+}
