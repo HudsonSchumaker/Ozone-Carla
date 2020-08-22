a6c89a133e4362a0e258fd8c82278ac5746f3e50
Schumaker <hudson@schumaker.com.br>
2020-08-22 17:14:40 +0200

    Add ILexer interface.

---
+++ src/br/com/schumaker/carla/lexer/ILexer.java
@@ -0,0 +1,16 @@
+package br.com.schumaker.carla.lexer;
+
+import br.com.schumaker.carla.files.O3File;
+import br.com.schumaker.carla.lexer.o3.O3Atom;
+import br.com.schumaker.carla.lexer.o3.O3AtomVariableTable;
+import br.com.schumaker.carla.lexer.o3.O3Function;
+import java.util.List;
+
+/**
+ *
+ * @author schumaker
+ */
+public interface ILexer {
+    O3Atom createProgram(O3File o3File);
+    O3AtomVariableTable fillAtomVariableTable(List<O3Function> functions);
+}
