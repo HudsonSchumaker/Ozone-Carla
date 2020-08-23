8ccb53654bf4bbf2bd8b94dfb783a79e71afcdb2
Schumaker <hudson@schumaker.com.br>
2020-08-23 13:53:22 +0200

    Extended O3SyntaxLibrary class.

--- src/br/com/schumaker/carla/o3/O3SyntaxLibrary.java
+++ src/br/com/schumaker/carla/o3/O3SyntaxLibrary.java
@@ -9,10 +9,17 @@ import java.util.List;
  */
 public class O3SyntaxLibrary {
     
-    public static final List<String> LIB_ID = Arrays.asList("STR_", "PRT_", "MATH_");
+    public static final List<String> LIB_ID = Arrays.asList("STR_", "PRT_", "MATH_", "FILE");
+   
+    // String lib
     public static final String STRINGS_O = "O3String.o";
     public static final String STRINGS_C = "O3String.c";
     
+    // Print lib
     public static final String PRINT_O = "O3Print.o";
-    public static final String PRINT_C = "O3Print.c";    
+    public static final String PRINT_C = "O3Print.c";
+
+    // File lib
+    public static final String FILE_O = "O3File.o";
+    public static final String FILE_C = "O3File.c";
 }
