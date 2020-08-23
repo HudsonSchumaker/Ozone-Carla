440a1ecfdb19b8b19f2f605aa821940eab7dbbcb
Schumaker <hudson@schumaker.com.br>
2020-08-24 00:12:48 +0200

    Added O3AsmSectionData class.

---
+++ src/br/com/schumaker/carla/files/O3AsmSectionData.java
@@ -0,0 +1,24 @@
+package br.com.schumaker.carla.files;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+@Data
+public class O3AsmSectionData implements IO3AsmFileSection {
+
+    public static final String SECTION_DATA = "section .data";
+    private List<String> sectionDataLines = new ArrayList<>();
+    
+    public O3AsmSectionData() {
+        sectionDataLines.add(SECTION_DATA);
+    }
+
+    public void addLine(String line) {
+        this.sectionDataLines.add("\t" + line);
+    }
+}
