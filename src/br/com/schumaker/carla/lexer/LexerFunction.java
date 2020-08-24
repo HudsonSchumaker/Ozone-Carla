c8a8999f1f674edf4b45f4387b989932189ea6bd
Schumaker <hudson@schumaker.com.br>
2020-08-25 00:09:28 +0200

    Changes in LexerFunction class.

--- src/br/com/schumaker/carla/lexer/LexerFunction.java
+++ src/br/com/schumaker/carla/lexer/LexerFunction.java
@@ -5,7 +5,7 @@ import br.com.schumaker.carla.files.O3FileLine;
 import br.com.schumaker.carla.lexer.o3.O3Function;
 import br.com.schumaker.carla.lexer.o3.O3FunctionTable;
 import br.com.schumaker.carla.lexer.o3.O3FunctionVariableTable;
-import br.com.schumaker.carla.o3.O3CoreKeyword;
+import br.com.schumaker.carla.o3.O3SyntaxKeyword;
 import br.com.schumaker.carla.lexer.o3.O3Statement;
 import br.com.schumaker.carla.lexer.o3.O3Variable;
 import br.com.schumaker.carla.lexer.o3.O3VariableTable;
@@ -18,35 +18,35 @@ import lombok.NoArgsConstructor;
 
 /**
  * This class create the O³ functions representations.
- * @author schumaker
+ *
+ * @author Hudson Schumaker
  */
-
 @Data
 @NoArgsConstructor
 public class LexerFunction {
-    
+
     private O3FunctionTable o3FunctionTable = new O3FunctionTable();
-    
+
     public List<O3Function> getFunctions(O3File file) {
         var headerLines = this.getHeaderLines(file);
         var functions = new ArrayList<O3Function>();
-       
+
         for (O3FileLine line : headerLines) {
             functions.add(this.setStatement(line, file));
         }
-           
+
         var lexerStatement = new LexerStatement();
         lexerStatement.getFunctionStatements(functions);
-     
+
         return functions;
     }
-    
+
     /**
      * This method create O3Function object with a basic O3Statement and
      * O3VariableTable, in the next step when is create the O3FunctionStament
      * the O3FunctionVariableTable should be created.
-     * 
-     * @param headerLine 
+     *
+     * @param headerLine
      * @param file O3File, source file .o3
      * @return O3Function, representation of a function.
      */
@@ -54,23 +54,23 @@ public class LexerFunction {
         var endBlock = "";
         var k = headerLine.getInternalNumber();
         var functionLines = new ArrayList<O3FileLine>();
-        while(!endBlock.equals(O3CoreKeyword.CLOSE_STATEMENT)) {
+        while (!endBlock.equals(O3SyntaxKeyword.CLOSE_STATEMENT)) {
             functionLines.add(file.getLines().get(k));
             endBlock = file.getLines().get(k).getData();
             k++;
         }
-       
-        var function = new O3Function (
+
+        var function = new O3Function(
                 this.getFunctionName(headerLine.getData()),
                 this.getFunctionInternalName(headerLine.getData()),
-                this.isMainFunction(headerLine), 
+                this.isMainFunction(headerLine),
                 this.getParams(this.getFunctionName(headerLine.getData()), headerLine),
                 new O3Statement(functionLines),
                 new O3VariableTable(functionLines));
         this.o3FunctionTable.add(function); // see if the reference will be update.
         return function; // debug purpose
     }
-    
+
     public List<O3FileLine> getHeaderLines(O3File file) {
         var headerLines = new ArrayList<O3FileLine>();
         for (O3FileLine line : file.getLines()) {
@@ -80,29 +80,29 @@ public class LexerFunction {
         }
         return headerLines;
     }
-    
+
     public String getFunctionName(String data) {
-        var name = data.substring(O3CoreKeyword.FUNCTION.length(), data.length()).trim();
-        name = name.substring(0, name.indexOf(O3CoreKeyword.OPEN_EXPRESSION));
+        var name = data.substring(O3SyntaxKeyword.FUNCTION.length(), data.length()).trim();
+        name = name.substring(0, name.indexOf(O3SyntaxKeyword.OPEN_EXPRESSION));
         return name;
     }
-    
+
     public String getFunctionInternalName(String data) {
         return "_" + this.getFunctionName(data) + ":";
     }
-    
+
     public boolean isMainFunction(O3FileLine headerLine) {
         return headerLine.getData().contains("f: main()");
     }
-    
+
     public List<O3Variable> getParams(String functionName, O3FileLine line) {
         return new LexerVariable().getParameters(functionName, line);
     }
-    
+
     public O3FunctionVariableTable createVariableTable(String functionName, List<O3FileLine> lines) {
         var lexerVariable = new LexerVariable();
-        var variables = lexerVariable.getVariables(functionName, lines); 
-        
+        var variables = lexerVariable.getVariables(functionName, lines);
+
         Set<O3Variable> funcVars = new HashSet<>(variables);
         return new O3FunctionVariableTable(funcVars);
     }
