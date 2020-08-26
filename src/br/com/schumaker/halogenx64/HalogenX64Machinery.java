ee40ab816f73a6a0eb6a6e9dbc1244e1e50fd299
Schumaker <hudson@schumaker.com.br>
2020-08-27 00:46:05 +0200

    Changes on HalogenX64Machinery class.

--- src/br/com/schumaker/halogenx64/HalogenX64Machinery.java
+++ src/br/com/schumaker/halogenx64/HalogenX64Machinery.java
@@ -3,6 +3,7 @@ package br.com.schumaker.halogenx64;
 import br.com.schumaker.carla.files.O3AsmFile;
 import br.com.schumaker.carla.io.AsmFileWriter;
 import br.com.schumaker.carla.lexer.o3.O3Atom;
+import br.com.schumaker.carla.lexer.o3.O3Function;
 import br.com.schumaker.carla.lexer.o3.O3Variable;
 
 /**
@@ -11,9 +12,9 @@ import br.com.schumaker.carla.lexer.o3.O3Variable;
  */
 public class HalogenX64Machinery {
 
-    private static final String SPACES_2 = "  ";
-    private final X64O3Variable2AsmMapper mapper = new X64O3Variable2AsmMapper();
+    public static final String SPACES_2 = "  ";
     private final AsmFileWriter writer;
+    private final HalogenX64Function hx64Fucntion = new HalogenX64Function();
 
     private String workspace;
     private O3Atom o3Atom;
@@ -28,13 +29,30 @@ public class HalogenX64Machinery {
 
     public void processO3AsmFile() {
         this.createInitializedData();
+        this.createUninitilizedData();
+        this.createSectionText();
         this.writeFile();
     }
 
     public void createInitializedData() {
         var variables = o3Atom.getVaribleTable().getVariables();
         for (var var : variables) {
-            this.resolveTypeValueAndAdd(var);
+            this.resolveVaribleTypeValueAndAdd(var);
+        }
+    }
+    
+    public void createUninitilizedData() {
+        //some code goes here...
+    }
+    
+    public void createSectionText() {
+        var functions = o3Atom.getFunctions();
+        for (var func : functions) {
+            if (func.isMain()) {
+                this.resolveFunctionMainAndAdd(func);
+            } else {
+                this.resolveFunctionAndAdd(func);
+            }
         }
     }
 
@@ -42,26 +60,16 @@ public class HalogenX64Machinery {
        writer.write(o3AsmFile);
     }
 
-    public void resolveTypeValueAndAdd(O3Variable o3Var) {
-        StringBuffer buffer = new StringBuffer();
-
-        // internal name
-        buffer.append(o3Var.getInternalName());
-        buffer.append(SPACES_2);
-
-        // type
-        buffer.append(mapper.getAsmType(o3Var.getTypeValue().getType().getName()));
-        buffer.append(SPACES_2);
-
-        // value
-        var value = o3Var.getTypeValue().getValue();
-        if (value instanceof String) {
-            buffer.append(value);
-            buffer.append(" , 0x0");
-        } else {
-            buffer.append(value);
-        }
-        buffer.append("\n");
-        this.o3AsmFile.getSectionData().addLine(buffer.toString());
+    public void resolveVaribleTypeValueAndAdd(O3Variable o3Var) {
+        Halogenx64Variable hx64Variable = new Halogenx64Variable();
+        this.o3AsmFile.getSectionData().addLine(hx64Variable.resolveTypeValue(o3Var));
+    }
+    
+    public void resolveFunctionMainAndAdd(O3Function o3Func) {
+        this.o3AsmFile.getSectionText().addLine(hx64Fucntion.resolveFunctionMain(o3Func));
+    }
+    
+    public void resolveFunctionAndAdd(O3Function o3Func) {
+        this.o3AsmFile.getSectionText().addLine(hx64Fucntion.resolveFunction(o3Func));
     }
 }
