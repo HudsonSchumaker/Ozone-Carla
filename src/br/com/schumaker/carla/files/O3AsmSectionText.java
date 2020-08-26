6da067137d2d11536a5e8b476ac6b57505c8be4c
Schumaker <hudson@schumaker.com.br>
2020-08-27 00:23:06 +0200

    Changes on O3AsmSectionText class.

--- src/br/com/schumaker/carla/files/O3AsmSectionText.java
+++ src/br/com/schumaker/carla/files/O3AsmSectionText.java
@@ -10,30 +10,45 @@ import lombok.Data;
  */
 @Data
 public class O3AsmSectionText {
+
     public static final String SECTION_TEXT = "\nsection .data";
     private List<String> sectionTextLines = new ArrayList<>();
-    
+    private List<String> exitCallLines = new ArrayList<>();
+
     public O3AsmSectionText() {
-       this.sectionTextLines.add(SECTION_TEXT);
-       this.addEntryPoint();
-       this.addMainFunction();
-       this.setUpStack();
+        this.sectionTextLines.add(SECTION_TEXT);
+        this.addEntryPoint();
+        this.addMainFunction();
+        this.setUpStack();
+        this.setUpExitCall();    
     }
     
+    public void addLine(String line) {
+        sectionTextLines.add("\t" + line);
+    }
+
     public void addEntryPoint() {
         this.sectionTextLines.add("\n\tglobal _main");
     }
-    
+
     public void addMainFunction() {
         this.sectionTextLines.add("\n\n");
         this.sectionTextLines.add("_main:");
         this.sectionTextLines.add("\n");
     }
-    
+
     public void setUpStack() {
         this.sectionTextLines.add("\n");
         this.sectionTextLines.add("\tpush      rbp");
         this.sectionTextLines.add("\n");
         this.sectionTextLines.add("\tmov       rbp, rsp");
+    }
+
+    public void setUpExitCall() {
+        this.exitCallLines.add("\n");
+        this.exitCallLines.add("\tmov     rax, 0x02000001");
+        this.exitCallLines.add("\txor     rdi, rdi");
+        this.exitCallLines.add("\tsyscall");
+        this.exitCallLines.add("\n");
     }
 }
