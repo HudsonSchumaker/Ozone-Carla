83de208fec72769547a3d07968e1344ec51fee28
Schumaker <hudson@schumaker.com.br>
2020-08-28 00:44:01 +0200

    Added on X128RegisterNotFound class.

---
+++ src/br/com/schumaker/carla/exception/X128RegisterNotFound.java
@@ -0,0 +1,14 @@
+package br.com.schumaker.carla.exception;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public class X128RegisterNotFound extends RuntimeException {
+
+    private static final String MESSAGE = "128 bits register not found.";
+
+    public X128RegisterNotFound() {
+        super(MESSAGE);
+    }
+}
