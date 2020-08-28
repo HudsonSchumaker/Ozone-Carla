270825ec1ee62269f1b1effbeebced3cd2cdba80
Schumaker <hudson@schumaker.com.br>
2020-08-29 01:01:37 +0200

    Added ArgumentTypeNotSupportedException class.Change

--- src/br/com/schumaker/carla/exception/ArgumentTypeNotSupportedException.java
+++ src/br/com/schumaker/carla/exception/ArgumentTypeNotSupportedException.java
@@ -5,7 +5,9 @@ package br.com.schumaker.carla.exception;
  * @author Hudson Schumaker
  */
 public class ArgumentTypeNotSupportedException extends RuntimeException {
+
     private static final String MESSAGE = "Wrong argument type. Please read the documentation.";
+
     public ArgumentTypeNotSupportedException() {
         super(MESSAGE);
     }
