b1bb091071f844e6f9b9f76c790ba5028f9675fb
Schumaker <hudson@schumaker.com.br>
2020-08-29 01:03:49 +0200

    Added CopyNASM class.Change

--- src/br/com/schumaker/carla/io/CopyNASM.java
+++ src/br/com/schumaker/carla/io/CopyNASM.java
@@ -1,25 +1,36 @@
 package br.com.schumaker.carla.io;
 
-import java.io.IOException;
+import br.com.schumaker.carla.exception.CopyNASMZipFileException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 /**
  * This class contains method(s) to get NASM lib file.
- * @author schumaker
+ *
+ * @author Hudson Schumaker
  */
 public final class CopyNASM {
-    
+
+    public static final String NASM_ZIP = "nasm.zip";
     public static final String NASM_ZIP_PATH = "/resource/nasm.zip";
 
     /**
-     * This method is responsible for copy the NASM zipped compiler file. 
-     * @throws IOException
+     * This method is responsible for copy the NASM zipped compiler file.
+     *
+     * @param atomName
      */
-    public void copyNASMZipFile() throws IOException {
-        var origin = getClass().getResourceAsStream(NASM_ZIP_PATH);
-        var destination = Path.of(System.getProperty("user.dir") + "/nasm.zip");
-        Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);
+    public void copyNASMZipFile(String atomName) {
+        try {
+            var origin = getClass().getResourceAsStream(NASM_ZIP_PATH);
+            var destination = Path.of(System.getProperty("user.dir")
+                    + System.getProperty("file.separator")
+                    + atomName
+                    + System.getProperty("file.separator")
+                    + NASM_ZIP);
+            Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            throw new CopyNASMZipFileException();
+        }
     }
 }
