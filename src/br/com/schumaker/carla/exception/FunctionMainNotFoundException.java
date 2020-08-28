9c73ce0e6908dc0262538ebc04cf01b241de2cb4
Schumaker <hudson@schumaker.com.br>
2020-08-29 01:01:54 +0200

    Added FunctionMainNotFoundException class.Change

--- src/br/com/schumaker/carla/exception/FunctionMainNotFoundException.java
+++ src/br/com/schumaker/carla/exception/FunctionMainNotFoundException.java
@@ -2,12 +2,13 @@ package br.com.schumaker.carla.exception;
 
 /**
  *
- * @author schumaker
+ * @author Hudson Schumaker
  */
 public class FunctionMainNotFoundException extends RuntimeException {
-    private static final String MESSAGE = "Create a main funciton. Ex: f: main() {}"; 
-    
+
+    private static final String MESSAGE = "Create a main funciton. Ex: f: main() {}";
+
     public FunctionMainNotFoundException() {
         super(MESSAGE);
-    }   
+    }
 }
