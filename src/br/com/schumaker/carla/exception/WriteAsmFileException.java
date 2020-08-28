b62cf6ab0c9f398f13b94b5f814eaedd6af9b5d6
Schumaker <hudson@schumaker.com.br>
2020-08-29 01:02:13 +0200

    Added WriteAsmFileException class.Change

--- src/br/com/schumaker/carla/exception/WriteAsmFileException.java
+++ src/br/com/schumaker/carla/exception/WriteAsmFileException.java
@@ -5,6 +5,7 @@ package br.com.schumaker.carla.exception;
  * @author Hudson Schumaker
  */
 public class WriteAsmFileException  extends RuntimeException {
+    
     private static final String MESSAGE = "Write Asm file error.";
     
     public WriteAsmFileException() {
