67b40d030e139c121120811cf3f5d66c375b8ea2
Schumaker <hudson@schumaker.com.br>
2020-08-31 00:06:39 +0200

    updated CopyO3aLib class.

--- src/br/com/schumaker/carla/io/CopyO3aLib.java
+++ src/br/com/schumaker/carla/io/CopyO3aLib.java
@@ -7,19 +7,21 @@ import java.nio.file.StandardCopyOption;
 
 /**
  * This class contains method(s) to get ASM lib file(s).
- * @author schumaker
+ *
+ * @author Hudson Schumaker
  */
 public class CopyO3aLib {
-    
-    public static final String C_LIB_A_PATH = "/resource/a";    
-    
+
+    public static final String C_LIB_A_PATH = "/resource/a";
+
     /**
-     * This method is responsible for copy the NASM source .asm file. 
+     * This method is responsible for copy the NASM source .asm file.
+     *
      * @throws IOException
      */
     public void copyAByName(String name) throws IOException {
         var origin = getClass().getResourceAsStream(C_LIB_A_PATH + "/" + name);
         var destination = Path.of(System.getProperty("user.dir") + "/" + name);
-        Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);    
+        Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);
     }
 }
