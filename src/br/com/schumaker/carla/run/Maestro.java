4cb3f7bd1a486c2b85c137b23b8f309beca3a61a
Schumaker <hudson@schumaker.com.br>
2020-08-31 00:01:49 +0200

    Update run package.

--- src/br/com/schumaker/carla/run/Maestro.java
+++ src/br/com/schumaker/carla/run/Maestro.java
@@ -4,34 +4,46 @@ import br.com.schumaker.carla.files.O3File;
 import br.com.schumaker.carla.files.O3FileBuilder;
 import br.com.schumaker.carla.lexer.Lexer;
 import br.com.schumaker.carla.lexer.RawSourceFileChecker;
+import br.com.schumaker.carla.lexer.o3.O3Atom;
+import br.com.schumaker.halogenx64.HalogenX64;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * This class orchestrates all the compilation process. 
- * @author schumaker
+ * This class orchestrates all the compilation process.
+ *
+ * @author Hudson Schumaker
  */
 public class Maestro {
 
     private final List<String> args;
-
+    
     public Maestro(String[] args) {
         this.args = Arrays.asList(args);
     }
 
     public void start() throws Exception {
         var o3File = this.initializeO3File(args.get(0));
-       
+
         var lexer = new Lexer();
         var atom = lexer.createProgram(o3File);
         System.out.println(atom);
         
+       // this.machineCode(atom);
     }
     
-    public void start(String something) {
+    public void machineCode(O3Atom atom) throws IOException {
+        var halogenX64 = new HalogenX64();
+        halogenX64.processO3Atom(atom);
+    }
 
-    }
-    
+    /**
+     * Create a representation of a .o3 source file in memory.
+     * @param path path to the physical file source code (.o3)
+     * @return O3File
+     * @throws Exception 
+     */
     public O3File initializeO3File(String path) throws Exception {
         var fileBuilder = new O3FileBuilder();
         var file = fileBuilder.build(path);
