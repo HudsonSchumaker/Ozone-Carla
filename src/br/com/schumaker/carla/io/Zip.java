52b23d639217b7cdf81eb59d812540fb20768311
Schumaker <hudson@schumaker.com.br>
2020-08-31 00:06:09 +0200

    updated Zip class.

--- src/br/com/schumaker/carla/io/Zip.java
+++ src/br/com/schumaker/carla/io/Zip.java
@@ -9,50 +9,77 @@ import java.util.zip.ZipInputStream;
 
 /**
  *
- * @author schumaker
+ * @author Hudson Schumaker
  */
 public final class Zip {
-    
+
     /**
-     * This is method is responsible for unzip the NASM compiler file. 
-     * @throws IOException 
+     * This is method is responsible for unzip the NASM compiler file.
+     *
+     * @param atomName
+     * @throws IOException
      */
-    public void unzipFile() throws IOException {
-        this.createNASMDir();
-        
-        var nasmZip = System.getProperty("user.dir") + "/nasm.zip";
-        var destDir = new File(System.getProperty("user.dir") + "/nasm");
-        
-        byte[] buffer = new byte[1024];
-        var zis = new ZipInputStream(new FileInputStream(nasmZip));
-        var zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-            var newFile = this.newFile(destDir, zipEntry);
-            var fos = new FileOutputStream(newFile);
-            int length;
-            while ((length = zis.read(buffer)) > 0) {
-                fos.write(buffer, 0, length);
+    public void unzipFile(String atomName) throws IOException {
+        this.createNASMDir(atomName);
+        var nasmZip = this.createVirtualZipPath(atomName);
+        var destDir = this.createDestinationDir(atomName);
+
+        FileInputStream fis;
+        byte[] buffer = new byte[2048];
+        try {
+            fis = new FileInputStream(nasmZip);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+
+                if (!ze.isDirectory()) {
+                    new File(newFile.getParent()).mkdirs();
+                    FileOutputStream fos = new FileOutputStream(newFile);
+
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                    fos.close();
+                } else {
+                    newFile.mkdir();
+                }
+
+                zis.closeEntry();
+                ze = zis.getNextEntry();
             }
-            fos.close();
-            zipEntry = zis.getNextEntry();
+
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            throw e;
         }
-        zis.closeEntry();
-        zis.close();
     }
-    
-    public File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
-        var destFile = new File(destinationDir, zipEntry.getName());
-        var destDirPath = destinationDir.getCanonicalPath();
-        var destFilePath = destFile.getCanonicalPath();
+
+    public void createNASMDir(String workspace) {
+        var dir = new File(System.getProperty("user.dir")
+                + System.getProperty("file.separator")
+                + workspace
+                + System.getProperty("file.separator")
+                + "nasm");
         
-        if (!destFilePath.startsWith(destDirPath + File.separator)) {
-            throw new IOException("outside of target dir: " + zipEntry.getName());
-        }
-        return destFile;
-    }
-    
-    public void createNASMDir() {
-        var dir = new File(System.getProperty("user.dir") + "/nasm");
         dir.mkdir();
+    }
+
+    public String createVirtualZipPath(String workspace) {
+        return System.getProperty("user.dir")
+                + System.getProperty("file.separator")
+                + workspace
+                + System.getProperty("file.separator")
+                + "nasm.zip";
+    }
+
+    public File createDestinationDir(String workspace) {
+        return new File(System.getProperty("user.dir")
+                + System.getProperty("file.separator")
+                + workspace);
     }
 }
