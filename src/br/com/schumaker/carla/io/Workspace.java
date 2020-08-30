4cf9ab9c131ec48e61062219a7094fde7929e943
Schumaker <hudson@schumaker.com.br>
2020-08-31 00:06:01 +0200

    updated Workspace class.

--- src/br/com/schumaker/carla/io/Workspace.java
+++ src/br/com/schumaker/carla/io/Workspace.java
@@ -3,14 +3,20 @@ package br.com.schumaker.carla.io;
 import java.io.File;
 
 /**
+ * Creates a directory to be used as workspace for the atom program.
  *
- * @author schumaker
+ * @author Hudson Schumaker
  */
 public class Workspace {
+
+    public static boolean createWorkspace(String path, String atomName) {
+        var file = new File(path + System.getProperty("file.separator") + atomName);
+        return file.mkdir();
+    }
     
-    public boolean createWorkspace(String atomName) {
+    public static boolean createWorkspace(String atomName) {
         var path = System.getProperty("user.dir");
-        var file = new File(path  + System.getProperty("file.separator") + atomName);
+        var file = new File(path + System.getProperty("file.separator") + atomName);
         return file.mkdir();
     }
 }
