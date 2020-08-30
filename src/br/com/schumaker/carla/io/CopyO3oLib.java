e8f90c47c48e8c015ccfb5027e75d73807ab5376
Schumaker <hudson@schumaker.com.br>
2020-08-31 00:06:26 +0200

    updated CopyO3oLib class.

--- src/br/com/schumaker/carla/io/CopyO3oLib.java
+++ src/br/com/schumaker/carla/io/CopyO3oLib.java
@@ -7,19 +7,30 @@ import java.nio.file.StandardCopyOption;
 
 /**
  * This class contains method(s) to get O lib file(s).
- * @author schumaker
+ *
+ * @author Hudson Schumaker
  */
 public class CopyO3oLib {
 
     public static final String C_LIB_O_PATH = "/resource/o";
+    private String atomName;
+
+    public CopyO3oLib(String atomName) {
+        this.atomName = atomName;
+    }
 
     /**
      * This method is responsible for copy the object(s) .o file.
+     *
      * @throws IOException
      */
     public void copyOByName(String name) throws IOException {
         var origin = getClass().getResourceAsStream(C_LIB_O_PATH + "/" + name);
-        var destination = Path.of(System.getProperty("user.dir") + "/" + name);
+        var destination = Path.of(System.getProperty("user.dir")
+                + System.getProperty("file.separator")
+                + atomName
+                + System.getProperty("file.separator")
+                + name);
         Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);
     }
 }
