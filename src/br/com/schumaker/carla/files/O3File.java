e06afaca3715dee714b541ccd4d2c86aac7ddac2
Schumaker <hudson@schumaker.com.br>
2020-09-01 00:03:17 +0200

    Changes on O3File class.

--- src/br/com/schumaker/carla/files/O3File.java
+++ src/br/com/schumaker/carla/files/O3File.java
@@ -4,17 +4,17 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/** 
- *  This class holds the O3 file source.
- *  @author Hudson Schumaker
+/**
+ * This class holds the O3 file source.
+ *
+ * @author Hudson Schumaker
  */
-
 @Data
 @AllArgsConstructor
 public class O3File {
-    
+
     public static final String EXT = "o3";
-    
+
     private String name;
     private String path;
     private List<O3FileLine> lines;
