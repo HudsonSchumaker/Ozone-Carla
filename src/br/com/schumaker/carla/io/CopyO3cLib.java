627b34805f18125e699d9cf77466bf767f4e1ee9
Schumaker <hudson@schumaker.com.br>
2020-09-01 00:03:32 +0200

    Changes on CopyO3cLib class.

--- src/br/com/schumaker/carla/io/CopyO3cLib.java
+++ src/br/com/schumaker/carla/io/CopyO3cLib.java
@@ -7,19 +7,21 @@ import java.nio.file.StandardCopyOption;
 
 /**
  * This class contains method(s) to get C lib file(s).
+ *
  * @author schumaker
  */
 public class CopyO3cLib {
-    
-    public static final String C_LIB_C_PATH = "/resource/c";    
-    
+
+    public static final String C_LIB_C_PATH = "/resource/c";
+
     /**
-     * This method is responsible for copy the c source .c file. 
+     * This method is responsible for copy the c source .c file.
+     *
      * @throws IOException
      */
     public void copyCByName(String name) throws IOException {
         var origin = getClass().getResourceAsStream(C_LIB_C_PATH + "/" + name);
         var destination = Path.of(System.getProperty("user.dir") + "/" + name);
-        Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);    
+        Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING);
     }
 }
