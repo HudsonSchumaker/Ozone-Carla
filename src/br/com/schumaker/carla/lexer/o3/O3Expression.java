e402a6d129697d492224f203ef4c176f881c8354
Schumaker <hudson@schumaker.com.br>
2020-09-03 19:48:44 +0200

    Changes on O3Expression class.

--- src/br/com/schumaker/carla/lexer/o3/O3Expression.java
+++ src/br/com/schumaker/carla/lexer/o3/O3Expression.java
@@ -3,7 +3,7 @@ package br.com.schumaker.carla.lexer.o3;
 /**
  * This class represents the expressions of O³ pl.
  * Ex: (1 = 0)
- * @author schumaker
+ * @author Hudson Schumaker
  */
 public class O3Expression {
     
