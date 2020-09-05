40abcbe631000bbe3c0dd08119220faafd4ce8bd
Schumaker <hudson@schumaker.com.br>
2020-09-05 15:11:13 +0200

    Changes on O3SyntaxType class.

--- src/br/com/schumaker/carla/o3/O3SyntaxType.java
+++ src/br/com/schumaker/carla/o3/O3SyntaxType.java
@@ -18,7 +18,8 @@ public enum O3SyntaxType {
     LONG(8),    // quadword,   8bytes (dq), 64bits
     FLOAT(4),   // doubleword, 4bytes (dd), 32bits
     DOUBLE(8),  // quadword,   8bytes (dq), 64bits
-    PARAM(8);
+    PARAM(8),   // quadword,   8bytes (dq), 64bits
+    RETURN(8);  // quadword,   8bytes (dq), 64bits
 
     private final int size; // size in bytes
     private O3SyntaxType(int size) {
