e8f2ac531c397f03861950542f4ce18000506826
Schumaker <hudson@schumaker.com.br>
2020-09-07 00:00:59 +0200

    Fix on Zsh interface

--- src/br/com/schumaker/carla/build/Zsh.java
+++ src/br/com/schumaker/carla/build/Zsh.java
@@ -1,7 +1,7 @@
 package br.com.schumaker.carla.build;
 
 /**
- * Interface contract for create a Zsh terminal command executer.
+ * Interface contract for create a Zsh terminal command executor.
  *
  * @author Hudson Schumaker
  */
