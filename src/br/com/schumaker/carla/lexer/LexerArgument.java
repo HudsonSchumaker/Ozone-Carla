3eb8f788ae915fa4bfe6f480e33a3710bc55f0bb
Schumaker <hudson@schumaker.com.br>
2020-09-08 00:09:18 +0200

    Fix LexerArgument class.

--- src/br/com/schumaker/carla/lexer/LexerArgument.java
+++ src/br/com/schumaker/carla/lexer/LexerArgument.java
@@ -12,29 +12,39 @@ import br.com.schumaker.carla.o3.O3SyntaxKeyword;
  */
 public class LexerArgument {
 
+    private static int counter = 0;
+
     public O3Argument getTypeAndValue(String data) {
         if (data.contains("\"")) {
-            return new O3Argument(O3Argument.NAME,
-                    false, O3TypeValue.of(O3VariableType.STRING, data));
+            return new O3Argument(O3Argument.NAME + "_" + counter++,
+                    false, true, O3TypeValue.of(O3VariableType.STRING, data));
         }
 
         if (data.contains(O3SyntaxKeyword.TRUE) || data.contains(O3SyntaxKeyword.FALSE)) {
-            return new O3Argument(O3Argument.NAME,
-                    false, O3TypeValue.of(O3VariableType.BOOL, data));
+            return new O3Argument(O3Argument.NAME + "_" + counter++,
+                    false, true, O3TypeValue.of(O3VariableType.BOOL, data));
         }
 
         if (data.contains(O3SyntaxKeyword.FLOATING_POINT_SIGN)) {
             if (data.contains(O3SyntaxKeyword.FLOAT_SIGN)) {
-                return new O3Argument(O3Argument.NAME,
-                        false, O3TypeValue.of(O3VariableType.FLOAT, data));
+                return new O3Argument(O3Argument.NAME + "_" + counter++,
+                        false, true,
+                        O3TypeValue.of(O3VariableType.FLOAT,
+                                this.removeDecimalTypeSign(data)));
             }
             if (data.contains(O3SyntaxKeyword.DOUBLE_SIGN)) {
-                return new O3Argument(O3Argument.NAME,
-                        false, O3TypeValue.of(O3VariableType.DOUBLE, data));
+                return new O3Argument(O3Argument.NAME + "_" + counter++,
+                        false, true,
+                        O3TypeValue.of(O3VariableType.DOUBLE,
+                                this.removeDecimalTypeSign(data)));
             }
         }
 
-        return new O3Argument(O3Argument.NAME,
-                false, O3TypeValue.of(O3VariableType.INT, data));
+        return new O3Argument(O3Argument.NAME + "_" + counter++,
+                false, true, O3TypeValue.of(O3VariableType.INT, data));
+    }
+
+    public String removeDecimalTypeSign(String data) {
+        return data.substring(0, data.length() - 1);
     }
 }
