2096d8c967f9b4e54e153d9d72f10e630cb8dc2d
Schumaker <hudson@schumaker.com.br>
2020-09-09 00:29:00 +0200

    Change O3Variable class.

--- src/br/com/schumaker/carla/lexer/o3/O3Variable.java
+++ src/br/com/schumaker/carla/lexer/o3/O3Variable.java
@@ -18,9 +18,15 @@ public class O3Variable implements IO3Variable {
     private String internalName;
     private boolean isInitialized;
     private O3VariableTypeValue<O3VariableType, ?> typeValue;
+    
+    private O3Variable() {}
 
     @Override
     public O3VariableType getType() {
         return typeValue.getType();
+    }
+    
+    public static O3Variable getBlankO3Variable() {
+        return new O3Variable();
     }
 }
