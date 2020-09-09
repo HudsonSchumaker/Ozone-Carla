0fb217f1f1e0d32042fecb43b19889530a796668
Schumaker <hudson@schumaker.com.br>
2020-09-10 00:05:40 +0200

    Changes on UnsupportedNumberOfArgumentsException class.

--- src/br/com/schumaker/carla/exception/UnsupportedNumberOfArgumentsException.java
+++ src/br/com/schumaker/carla/exception/UnsupportedNumberOfArgumentsException.java
@@ -6,7 +6,7 @@ package br.com.schumaker.carla.exception;
  */
 public class UnsupportedNumberOfArgumentsException extends RuntimeException {
     
-    private static final String MESSAGE = "Number of arguments are not corret.";
+    private static final String MESSAGE = "Number of arguments are not correct.";
     
     public UnsupportedNumberOfArgumentsException() {
         super(MESSAGE);
