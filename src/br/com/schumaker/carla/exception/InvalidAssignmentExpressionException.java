1636238285459e456251e79c66a4cf521c64f7d0
Schumaker <hudson@schumaker.com.br>
2020-09-11 00:04:11 +0200

    Added InvalidAssignmentExpressionException class.

---
+++ src/br/com/schumaker/carla/exception/InvalidAssignmentExpressionException.java
@@ -0,0 +1,14 @@
+package br.com.schumaker.carla.exception;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public class InvalidAssignmentExpressionException extends RuntimeException {
+    
+    private static final String MESSAGE = "";  
+    
+    public InvalidAssignmentExpressionException() {
+        super(MESSAGE);
+    }
+}
