b55c8f21283b2bb6b737d0a855e450cd0b2eed64
Schumaker <hudson@schumaker.com.br>
2020-09-11 00:07:56 +0200

    added on O3Sub class.

---
+++ src/br/com/schumaker/carla/o3/core/O3Sub.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.schumaker.carla.o3.core;
+
+/**
+ *
+ * @author schumaker
+ */
+public class O3Sub {
+    
+}
