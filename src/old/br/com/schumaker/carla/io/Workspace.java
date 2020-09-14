dbaeef82d1ed36a0891c23299df939d51e49bdda
Schumaker <hudson@schumaker.com.br>
2020-09-14 20:36:34 +0200

    Changes on the project

---
+++ src/old/br/com/schumaker/carla/io/Workspace.java
@@ -0,0 +1,22 @@
+package old.br.com.schumaker.carla.io;
+
+import java.io.File;
+
+/**
+ * Creates a directory to be used as workspace for the atom program.
+ *
+ * @author Hudson Schumaker
+ */
+public class Workspace {
+
+    public static boolean createWorkspace(String path, String atomName) {
+        var file = new File(path + System.getProperty("file.separator") + atomName);
+        return file.mkdir();
+    }
+    
+    public static boolean createWorkspace(String atomName) {
+        var path = System.getProperty("user.dir");
+        var file = new File(path + System.getProperty("file.separator") + atomName);
+        return file.mkdir();
+    }
+}
---
+++ src/old/br/com/schumaker/carla/io/Writer.java
@@ -0,0 +1,9 @@
+package old.br.com.schumaker.carla.io;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public interface Writer {
+    void write(String fileName);
+}
