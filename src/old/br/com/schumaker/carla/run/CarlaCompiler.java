83bd808097ecd48d82786fe691cb10e081735d05
Schumaker <hudson@schumaker.com.br>
2020-09-14 20:37:13 +0200

    Changes on the project

---
+++ src/old/br/com/schumaker/carla/run/CarlaCompiler.java
@@ -0,0 +1,15 @@
+package old.br.com.schumaker.carla.run;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public class CarlaCompiler {
+//    public static void main(String[] args) throws Exception {
+//        if (args.length > 0) {
+//            new Maestro(args).start();
+//        } else {
+//            System.exit(1);
+//        }
+//    }
+}
---
+++ src/old/br/com/schumaker/carla/run/ParamsChecker.java
@@ -0,0 +1,9 @@
+package br.com.schumaker.carla.run;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public final class ParamsChecker {
+    
+}
