5a00680c77672cc69a9427c002e5967e4359bff4
Schumaker <hudson@schumaker.com.br>
2020-09-15 00:24:14 +0200

    Added DataStore interface

---
+++ src/br/com/schumaker/carla/io/o3/core/DataStore.java
@@ -0,0 +1,10 @@
+package br.com.schumaker.carla.io.o3.core;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public interface DataStore extends MemorySpace {
+    String getName();
+    String getOType(); 
+}
