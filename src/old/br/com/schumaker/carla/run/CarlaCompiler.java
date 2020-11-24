8d34fa3c84adea445c147387b4fa838fc5151429
HudsonSchumaker <hudson.schumaker@me.com>
2020-11-24 23:21:11 +0100

    New Libs

--- src/old/br/com/schumaker/carla/run/CarlaCompiler.java
+++ src/old/br/com/schumaker/carla/run/CarlaCompiler.java
@@ -5,11 +5,11 @@ package old.br.com.schumaker.carla.run;
  * @author Hudson Schumaker
  */
 public class CarlaCompiler {
-//    public static void main(String[] args) throws Exception {
-//        if (args.length > 0) {
-//            new Maestro(args).start();
-//        } else {
-//            System.exit(1);
-//        }
-//    }
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            new Maestro(args).start();
+        } else {
+            System.exit(1);
+        }
+    }
 }
