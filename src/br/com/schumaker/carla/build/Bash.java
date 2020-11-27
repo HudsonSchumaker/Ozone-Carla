52e4b6045563711504588ac202a35a61acd911d7
HudsonSchumaker <hudson.schumaker@me.com>
2020-11-27 18:13:23 +0100

    Put Back bash

--- src/br/com/schumaker/carla/build/Bash.java
+++ src/br/com/schumaker/carla/build/Bash.java
@@ -1,11 +1,6 @@
 package br.com.schumaker.carla.build;
 
-/**
- * Interface contract for create a Bash terminal command executor.
- *
- * @author Hudson Schumaker
- */
 public interface Bash {
-    
+
     void executeCommand(String[] commands);
 }
