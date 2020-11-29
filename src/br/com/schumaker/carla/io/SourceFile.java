d323ce85aa1613fd5b3c33ca35445b6dbd34f6c8
HudsonSchumaker <hudson.schumaker@me.com>
2020-11-29 13:12:25 +0100

    add Fileline to source file.

--- src/br/com/schumaker/carla/io/SourceFile.java
+++ src/br/com/schumaker/carla/io/SourceFile.java
@@ -1,11 +1,16 @@
 package br.com.schumaker.carla.io;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@AllArgsConstructor
 public class SourceFile {
     public static final String EXT = "o3";
 
     private String name;
     private String path;
+    private List<FileLine> lines;
 }
