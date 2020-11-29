15cb2be9673ccdbe13f5c9d22dfb1cc2d59131ab
HudsonSchumaker <hudson.schumaker@me.com>
2020-11-29 12:57:06 +0100

    Maestro

---
+++ src/br/com/schumaker/carla/run/Maestro.java
@@ -0,0 +1,23 @@
+package br.com.schumaker.carla.run;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class orchestrates all the compilation process.
+ *
+ * @author Hudson Schumaker
+ */
+public final class Maestro {
+
+    private final List<String> args;
+
+    public Maestro(String[] args) {
+        this.args = Arrays.asList(args);
+    }
+
+    public void start() {
+
+    }
+
+}
