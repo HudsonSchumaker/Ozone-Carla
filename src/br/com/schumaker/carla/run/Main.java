381bcce325537ecc33f8151ae17fc41a684f15b4
HudsonSchumaker <hudson.schumaker@me.com>
2020-11-29 12:56:57 +0100

    Maestro

--- src/br/com/schumaker/carla/run/Main.java
+++ src/br/com/schumaker/carla/run/Main.java
@@ -1,11 +1,14 @@
 package br.com.schumaker.carla.run;
 
 /**
- *
  * @author Hudson Schumaker
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Carla compiler");
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            new Maestro(args).start();
+        } else {
+            System.exit(1);
+        }
     }
 }
