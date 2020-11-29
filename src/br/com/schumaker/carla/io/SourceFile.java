5641e26c8f424917a139cecf6fcf149d4b049e51
HudsonSchumaker <hudson.schumaker@me.com>
2020-11-29 12:59:30 +0100

    Change names.

--- src/br/com/schumaker/carla/io/SourceFile.java
+++ src/br/com/schumaker/carla/io/SourceFile.java
@@ -3,7 +3,7 @@ package br.com.schumaker.carla.io;
 import lombok.Data;
 
 @Data
-public class O3SourceFile {
+public class SourceFile {
     public static final String EXT = "o3";
 
     private String name;
