1c2dc136cfa768311a352b5b675440cf21fd3ec4
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-03 20:15:23 +0100

    Add O3Keyword set.

---
+++ src/br/com/schumaker/carla/core/O3Keyword.java
@@ -0,0 +1,53 @@
+package br.com.schumaker.carla.core;
+
+/**
+ * This class defines the keywords of O³ p.l.
+ *
+ * @author Hudson Schumaker
+ */
+public final class O3Keyword {
+
+    private O3Keyword() {}
+
+    public static final String COMMENT = ";";
+    public static final String FUNCTION = "f:";
+    public static final String VARIABLE = "v:";
+    public static final String RETURN = "r:";
+
+    public static final String IF = "if";
+    public static final String ELSE = "else";
+    public static final String BREAK = "break";
+    public static final String CONTINUE = "continue";
+
+    public static final String WHILE = "while";
+    public static final String FOR = "for";
+    public static final String RANGE = "..";
+
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+    public static final String MULTIPLICATION = "*";
+    public static final String DIVISION = "/";
+
+    public static final String BIGGER = ">";
+    public static final String SMALLER = "<";
+    public static final String EQUAL = "==";
+    public static final String M_EQUAL = "===";
+    public static final String NOT_EQUAL = "!=";
+    public static final String ASSIGN = "=";
+    public static final String INC = "++";
+    public static final String DEC = "--";
+
+    public static final String OPEN_EXPRESSION = "(";
+    public static final String CLOSE_EXPRESSION = ")";
+
+    public static final String OPEN_STATEMENT = "{";
+    public static final String CLOSE_STATEMENT = "}";
+
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+    public static final String FLOATING_POINT_SIGN = ".";
+    public static final String FLOAT_SIGN = "f";
+    public static final String DOUBLE_SIGN = "d";
+
+
+}
