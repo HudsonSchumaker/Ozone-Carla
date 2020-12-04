18fb658fa27cf3550adfdeeee0db5caec279764c
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-04 11:03:27 +0100

    ILexer interface change.

--- src/br/com/schumaker/carla/lexer/Lexer.java
+++ src/br/com/schumaker/carla/lexer/Lexer.java
@@ -1,4 +1,13 @@
 package br.com.schumaker.carla.lexer;
 
+import br.com.schumaker.carla.o3.Atom;
+
+import java.io.File;
+
 public class Lexer implements ILexer {
+
+    @Override
+    public Atom createProgram(File file) {
+        return null;
+    }
 }
