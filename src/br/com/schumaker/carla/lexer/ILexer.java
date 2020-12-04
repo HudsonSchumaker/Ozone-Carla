f49cbf7f05d36ca51994f29b6b5ed657b5f4127f
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-04 11:03:14 +0100

    Lexer class change.

--- src/br/com/schumaker/carla/lexer/ILexer.java
+++ src/br/com/schumaker/carla/lexer/ILexer.java
@@ -1,9 +1,14 @@
 package br.com.schumaker.carla.lexer;
 
+import br.com.schumaker.carla.o3.Atom;
+
+import java.io.File;
+
 /**
  *
  * @author Hudson Schumaker
  */
 public interface ILexer {
 
+    Atom createProgram(File file);
 }
