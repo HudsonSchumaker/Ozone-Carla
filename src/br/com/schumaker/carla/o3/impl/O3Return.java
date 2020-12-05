1208f9c70f5f468bc29d2f428c530294339b2fc9
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-05 22:23:29 +0100

    Refactor.

---
+++ src/br/com/schumaker/carla/o3/impl/O3Return.java
@@ -0,0 +1,31 @@
+package br.com.schumaker.carla.o3.impl;
+
+import br.com.schumaker.carla.o3.Variable;
+
+public final class O3Return implements Variable {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getOType() {
+        return null;
+    }
+
+    @Override
+    public String getInternalName() {
+        return null;
+    }
+
+    @Override
+    public String getAType() {
+        return null;
+    }
+
+    @Override
+    public Object getValue() {
+        return null;
+    }
+}
