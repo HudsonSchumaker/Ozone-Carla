6536340c502ed957e65da3a6f0563ef3917ca23f
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-06 17:20:53 +0100

    changes O3Function class.

--- src/br/com/schumaker/carla/lexer/impl/LexerFunction.java
+++ src/br/com/schumaker/carla/lexer/impl/LexerFunction.java
@@ -1,9 +1,18 @@
 package br.com.schumaker.carla.lexer.impl;
 
+import br.com.schumaker.carla.io.impl.O3File;
 import br.com.schumaker.carla.lexer.ILexerFunction;
+import br.com.schumaker.carla.o3.impl.O3Function;
+
+import java.util.List;
 
 /**
  * @author Hudson Schumaker
  */
 public final class LexerFunction implements ILexerFunction {
+
+    @Override
+    public List<O3Function> getFunctions(O3File file) {
+        return null;
+    }
 }
