54439e7c47a0a2d5f62d0da3c384427d4555d6b8
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-06 17:21:14 +0100

    changes ILexerFunction interface.

--- src/br/com/schumaker/carla/lexer/ILexerFunction.java
+++ src/br/com/schumaker/carla/lexer/ILexerFunction.java
@@ -1,7 +1,14 @@
 package br.com.schumaker.carla.lexer;
 
+import br.com.schumaker.carla.io.impl.O3File;
+import br.com.schumaker.carla.o3.impl.O3Function;
+
+import java.util.List;
+
 /**
  * @author Hudson Schumaker
  */
 public interface ILexerFunction {
+
+    List<O3Function> getFunctions(O3File file);
 }
