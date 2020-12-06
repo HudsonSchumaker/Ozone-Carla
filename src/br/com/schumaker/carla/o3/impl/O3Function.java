568e5e61af64f7932ede057cdf753c7adba1ec3b
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-06 17:09:32 +0100

    changes O3Function class.

--- src/br/com/schumaker/carla/o3/impl/O3Function.java
+++ src/br/com/schumaker/carla/o3/impl/O3Function.java
@@ -1,4 +1,11 @@
 package br.com.schumaker.carla.o3.impl;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
 public class O3Function {
 }
