e7a36961759c5243fbecf47c4e0f392398a91bb3
Hudson Schumaker <hudson.schuamker@me.com>
2020-12-08 20:40:10 +0100

    test commit Pi

--- src/br/com/schumaker/carla/exception/ArgumentTypeNotSupportedException.java
+++ src/br/com/schumaker/carla/exception/ArgumentTypeNotSupportedException.java
@@ -6,6 +6,7 @@ package br.com.schumaker.carla.exception;
  */
 public class ArgumentTypeNotSupportedException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
     private static final String MESSAGE = "Wrong argument type. Please read the documentation.";
 
     public ArgumentTypeNotSupportedException() {
