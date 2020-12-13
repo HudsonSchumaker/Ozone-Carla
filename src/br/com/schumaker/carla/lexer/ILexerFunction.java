d6aaa1985ecdf14172ad877322457a32668cc707
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-13 17:32:53 +0100

    LexerFunction class and interface.

--- src/br/com/schumaker/carla/lexer/ILexerFunction.java
+++ src/br/com/schumaker/carla/lexer/ILexerFunction.java
@@ -3,6 +3,7 @@ package br.com.schumaker.carla.lexer;
 import br.com.schumaker.carla.io.impl.O3File;
 import br.com.schumaker.carla.io.impl.O3FileLine;
 import br.com.schumaker.carla.o3.impl.O3Function;
+import br.com.schumaker.carla.o3.impl.O3Parameter;
 
 import java.util.List;
 
@@ -13,7 +14,15 @@ public interface ILexerFunction {
 
     List<O3Function> getFunctions(O3File file);
 
-    String getFunctionName(String data);
+    List<O3Parameter> getParams(String functionName, O3FileLine line);
+
+    O3Function getBody(O3FileLine headerLine, O3File file);
+
+    List<O3FileLine> getHeaderLines(O3File file);
 
     boolean isMainFunction(O3FileLine headerLine);
+
+    String getFunctionInternalName(String data);
+
+    String getFunctionName(String data);
 }
--- src/br/com/schumaker/carla/lexer/impl/LexerFunction.java
+++ src/br/com/schumaker/carla/lexer/impl/LexerFunction.java
@@ -5,6 +5,7 @@ import br.com.schumaker.carla.io.impl.O3FileLine;
 import br.com.schumaker.carla.lexer.ILexerFunction;
 import br.com.schumaker.carla.o3.impl.O3Function;
 import br.com.schumaker.carla.o3.impl.O3Keyword;
+import br.com.schumaker.carla.o3.impl.O3Parameter;
 
 import java.util.List;
 
@@ -21,9 +22,29 @@ public final class LexerFunction implements ILexerFunction {
     }
 
     @Override
+    public List<O3Parameter> getParams(String functionName, O3FileLine line) {
+        return null;
+    }
+
+    @Override
+    public O3Function getBody(O3FileLine headerLine, O3File file) {
+        return null;
+    }
+
+    @Override
+    public List<O3FileLine> getHeaderLines(O3File file) {
+        return null;
+    }
+
+    @Override
     public String getFunctionName(String data) {
         var name = data.substring(O3Keyword.FUNCTION.length(), data.length()).trim();
         return name.substring(0, name.indexOf(O3Keyword.OPEN_EXPRESSION)).trim();
+    }
+
+    @Override
+    public String getFunctionInternalName(String data) {
+        return "_" + this.getFunctionName(data);
     }
 
     @Override
