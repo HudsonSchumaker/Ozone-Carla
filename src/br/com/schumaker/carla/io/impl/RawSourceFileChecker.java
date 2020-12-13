123ebe307dacb56f93c9eb94ed77166f3702a8f6
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-13 16:11:50 +0100

    RawSourceFileChecker changes.

--- src/br/com/schumaker/carla/io/impl/RawSourceFileChecker.java
+++ src/br/com/schumaker/carla/io/impl/RawSourceFileChecker.java
@@ -1,10 +1,12 @@
 package br.com.schumaker.carla.io.impl;
 
+import br.com.schumaker.carla.o3.impl.O3Keyword;
 import br.com.schumaker.carla.utils.StringUtils;
 
 import java.util.ArrayList;
 
 /**
+ * This class has methods do check and clean the source file.
  *
  * @author Hudson Schumaker
  */
@@ -15,22 +17,37 @@ public class RawSourceFileChecker {
         this.setInternalLineNumbers(file);
     }
 
+    /**
+     * Set the line numbers.
+     *
+     * @param file O3File source
+     */
     public void setInternalLineNumbers(O3File file) {
         for (int l = 0; l < file.getLines().size(); l++) {
             file.getLines().get(l).setInternalNumber(l);
         }
     }
 
+    /**
+     * Remove comment lines.
+     *
+     * @param file O3File source
+     */
     public void removeComments(O3File file) {
         var newLines = new ArrayList<O3FileLine>();
         for (O3FileLine line : file.getLines()) {
-            if (!line.getData().trim().startsWith(";")) {
+            if (!line.getData().trim().startsWith(O3Keyword.COMMENT)) {
                 newLines.add(line);
             }
         }
         file.setLines(newLines);
     }
 
+    /**
+     * Remove blank and empty lines.
+     *
+     * @param file O3File source
+     */
     public void removeBlankLines(O3File file) {
         var newLines = new ArrayList<O3FileLine>();
         for (O3FileLine line : file.getLines()) {
