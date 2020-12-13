52e5f1165da5190cc64e9921340a19fd939c26ee
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-13 17:32:31 +0100

    Variable table class and interface.

---
+++ src/br/com/schumaker/carla/o3/VariableTable.java
@@ -0,0 +1,7 @@
+package br.com.schumaker.carla.o3;
+
+/**
+ * @author Hudson Schumaker
+ */
+public interface VariableTable {
+}
---
+++ src/br/com/schumaker/carla/o3/impl/O3FunctionVariableTable.java
@@ -0,0 +1,9 @@
+package br.com.schumaker.carla.o3.impl;
+
+import br.com.schumaker.carla.o3.VariableTable;
+
+/**
+ * @author Hudson Schumaker
+ */
+public class O3FunctionVariableTable implements VariableTable {
+}
