186b919aa4aaaec7d91a3d81347da45aa499b79c
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-14 00:14:54 +0100

    changes on LexerFunctionTable class.

--- src/br/com/schumaker/carla/lexer/impl/LexerVariableTable.java
+++
@@ -1,9 +0,0 @@
-package br.com.schumaker.carla.lexer.impl;
-
-import br.com.schumaker.carla.lexer.ILexerVariableTable;
-
-/**
- * @author Hudson Schumaker
- */
-public class LexerVariableTable implements ILexerVariableTable {
-}
