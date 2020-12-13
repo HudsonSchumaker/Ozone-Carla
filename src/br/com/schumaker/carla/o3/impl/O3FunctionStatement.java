6e113a75732562aca8f98589782a18f82265a5b7
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-14 00:11:08 +0100

    add new class.

---
+++ src/br/com/schumaker/carla/o3/impl/O3FunctionStatement.java
@@ -0,0 +1,20 @@
+package br.com.schumaker.carla.o3.impl;
+
+import br.com.schumaker.carla.io.impl.O3FileLine;
+import br.com.schumaker.carla.o3.Statement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@ToString
+@AllArgsConstructor
+public class O3FunctionStatement implements Statement {
+
+    private List<O3Variable> variables;
+    //private List<O3FunctionCall> functionCalls;
+    private List<O3FileLine> lines;
+
+}
