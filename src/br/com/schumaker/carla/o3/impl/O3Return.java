b976f9eb03925283300f4a6e681fddf0bc20449a
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-15 20:41:42 +0100

    changes O3Return class.

--- src/br/com/schumaker/carla/o3/impl/O3Return.java
+++ src/br/com/schumaker/carla/o3/impl/O3Return.java
@@ -1,25 +1,29 @@
 package br.com.schumaker.carla.o3.impl;
 
 import br.com.schumaker.carla.o3.Variable;
+import br.com.schumaker.carla.o3.enums.MemorySpaceType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * @author Hudson Schumaker
  */
 @Data
+@AllArgsConstructor
 public final class O3Return implements Variable {
 
     private String name;
     private String internalName;
     private boolean constant;
-
-    @Override
-    public String getType() {
-        return null;
-    }
+    private MemorySpaceTypeValue<MemorySpaceType, ?> typeValue;
 
     @Override
     public Object getValue() {
-        return null;
+        return typeValue.getValue();
+    }
+
+    @Override
+    public String getType() {
+        return typeValue.getType().getName();
     }
 }
