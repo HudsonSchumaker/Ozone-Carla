fa49552bdd538e7a32c6ac77ab828b71637a4f31
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-15 20:32:46 +0100

    fix TerminalWinTest.

--- test/br/com/schumaker/carla/build/TerminalWinTest.java
+++ test/br/com/schumaker/carla/build/TerminalWinTest.java
@@ -1,15 +1,21 @@
 package br.com.schumaker.carla.build;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
+
+import br.com.schumaker.carla.utils.SystemUtils;
 import org.junit.Test;
 
 /**
- *
  * @author Hudson Schumaker
  */
 public class TerminalWinTest {
+
     @Test
     public void testExecuteCommand() throws InterruptedException {
+        // Verification
+        assumeTrue(SystemUtils.getOsName().contains("Windows"));
+
         // Preparation
         var path = System.getProperty("user.dir");
         var cmd = "pwd";
