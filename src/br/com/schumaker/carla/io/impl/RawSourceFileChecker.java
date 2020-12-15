b3278c1b635655e3293be22e1420840a98b74f0b
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-15 20:39:43 +0100

    changes RawSourceFileChecker class.

--- src/br/com/schumaker/carla/io/impl/RawSourceFileChecker.java
+++ src/br/com/schumaker/carla/io/impl/RawSourceFileChecker.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  * @author Hudson Schumaker
  */
 public class RawSourceFileChecker {
+
     public void doCheck(O3File file) {
         this.removeComments(file);
         this.removeBlankLines(file);
