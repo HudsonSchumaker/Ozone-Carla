524f17f9df2e556e9668773bc5fc4037b0b3d639
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-15 20:42:04 +0100

    changes O3Variable class.

--- src/br/com/schumaker/carla/o3/impl/O3Variable.java
+++ src/br/com/schumaker/carla/o3/impl/O3Variable.java
@@ -1,26 +1,30 @@
 package br.com.schumaker.carla.o3.impl;
 
 import br.com.schumaker.carla.o3.Variable;
+import br.com.schumaker.carla.o3.enums.MemorySpaceType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * @author Hudson Schumaker
  */
 @Data
+@AllArgsConstructor
 public final class O3Variable implements Variable {
 
     private String name;
     private String internalName;
     private boolean global;
     private boolean constant;
+    private MemorySpaceTypeValue<MemorySpaceType, ?> typeValue;
 
     @Override
     public Object getValue() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return typeValue.getValue();
     }
 
     @Override
     public String getType() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return typeValue.getType().getName();
     }
 }
