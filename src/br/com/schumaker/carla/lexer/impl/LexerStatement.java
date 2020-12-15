9572a2e22647f0b487e515ad775382a2582d0bae
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-15 21:10:55 +0100

    add ILexerStatement and LexerStatement.

---
+++ src/br/com/schumaker/carla/lexer/impl/LexerStatement.java
@@ -0,0 +1,11 @@
+package br.com.schumaker.carla.lexer.impl;
+
+import br.com.schumaker.carla.lexer.ILexerStatement;
+
+/**
+ * This class create the O³ functions statements representations.
+ *
+ * @author Hudson Schumaker
+ */
+public class LexerStatement implements ILexerStatement {
+}
