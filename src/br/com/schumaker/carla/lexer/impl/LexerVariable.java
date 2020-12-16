930021d51b43bcdeb738e8ece46297fb19c09d43
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-17 00:04:24 +0100

    Add.

---
+++ src/br/com/schumaker/carla/lexer/impl/LexerVariable.java
@@ -0,0 +1,11 @@
+package br.com.schumaker.carla.lexer.impl;
+
+import br.com.schumaker.carla.lexer.ILexerVariable;
+
+/**
+ * This class create the O³ variables representations.
+ *
+ * @author Hudson Schumaker
+ */
+public class LexerVariable implements ILexerVariable {
+}
