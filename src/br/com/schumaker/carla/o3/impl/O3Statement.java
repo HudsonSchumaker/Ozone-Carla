15b39eccd2983aaad795e721aea67e067a690743
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-17 00:02:45 +0100

    add toString().

--- src/br/com/schumaker/carla/o3/impl/O3Statement.java
+++ src/br/com/schumaker/carla/o3/impl/O3Statement.java
@@ -4,10 +4,12 @@ import br.com.schumaker.carla.io.impl.O3FileLine;
 import br.com.schumaker.carla.o3.Statement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
 @Data
+@ToString
 @AllArgsConstructor
 public class O3Statement implements Statement {
 
