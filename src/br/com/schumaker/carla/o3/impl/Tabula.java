9b01f4d6a01cca1ad1b307e28b02500e7cafff9f
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-17 00:02:24 +0100

    add toString().

--- src/br/com/schumaker/carla/o3/impl/Tabula.java
+++ src/br/com/schumaker/carla/o3/impl/Tabula.java
@@ -2,6 +2,7 @@ package br.com.schumaker.carla.o3.impl;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @author Hudson Schumaker
  */
 @Data
+@ToString
 @AllArgsConstructor
 public final class Tabula {
 
