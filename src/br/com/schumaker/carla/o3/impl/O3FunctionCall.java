e6d562287da070b13b80b94039ffe1c769064a75
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-17 00:04:07 +0100

    Add.

---
+++ src/br/com/schumaker/carla/o3/impl/O3FunctionCall.java
@@ -0,0 +1,14 @@
+package br.com.schumaker.carla.o3.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Hudson Schumaker
+ */
+@Data
+@ToString
+@AllArgsConstructor
+public class O3FunctionCall {
+}
