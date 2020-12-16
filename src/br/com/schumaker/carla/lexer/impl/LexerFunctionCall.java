c2fa9ea7e99f896d5a4bd8c3351188556f6b739a
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-17 00:04:29 +0100

    Add.

---
+++ src/br/com/schumaker/carla/lexer/impl/LexerFunctionCall.java
@@ -0,0 +1,11 @@
+package br.com.schumaker.carla.lexer.impl;
+
+import br.com.schumaker.carla.lexer.ILexerFunctionCall;
+
+/**
+ * This class deal with function calls inside a function statement.
+ *
+ * @author Hudson Schumaker
+ */
+public class LexerFunctionCall implements ILexerFunctionCall {
+}
