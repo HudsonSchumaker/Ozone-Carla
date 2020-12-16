021e99085ac29201f8bed2fad8400152737881e3
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-17 00:03:35 +0100

    add methods signatures to interface.

--- src/br/com/schumaker/carla/lexer/ILexerStatement.java
+++ src/br/com/schumaker/carla/lexer/ILexerStatement.java
@@ -1,7 +1,19 @@
 package br.com.schumaker.carla.lexer;
 
+import br.com.schumaker.carla.io.impl.O3FileLine;
+import br.com.schumaker.carla.o3.Statement;
+import br.com.schumaker.carla.o3.impl.O3Function;
+import br.com.schumaker.carla.o3.impl.O3Variable;
+
+import java.util.List;
+
 /**
  * @author Hudson Schumaker
  */
 public interface ILexerStatement {
+    void getFunctionStatements(List<O3Function> functions);
+
+    List<O3FileLine> getLinesWithVariableDeclaration(Statement statement);
+
+    List<O3Variable> getVariables(String functionName, List<O3FileLine> lines);
 }
