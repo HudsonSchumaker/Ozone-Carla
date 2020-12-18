4ac888e3b08b61b984a05c7b5b6bffb36c8350d2
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-18 15:52:15 +0100

    Changes Statement class.

--- src/br/com/schumaker/carla/o3/Statement.java
+++ src/br/com/schumaker/carla/o3/Statement.java
@@ -1,10 +1,12 @@
 package br.com.schumaker.carla.o3;
 
+import br.com.schumaker.carla.io.impl.O3FileLine;
+
 import java.util.List;
 
 /**
  * @author Hudson Schumaker
  */
 public interface Statement {
-    List<?> getLines();
+    List<O3FileLine> getLines();
 }
