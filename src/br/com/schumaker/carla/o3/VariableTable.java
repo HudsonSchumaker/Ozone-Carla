1ce5c13ddfd6635c4955bd2be0228f86bb559318
HudsonSchumaker <hudson.schumaker@me.com>
2020-12-18 15:51:52 +0100

    Changes VariableTable class

--- src/br/com/schumaker/carla/o3/VariableTable.java
+++ src/br/com/schumaker/carla/o3/VariableTable.java
@@ -1,6 +1,8 @@
 package br.com.schumaker.carla.o3;
 
 import br.com.schumaker.carla.o3.impl.O3Parameter;
+import br.com.schumaker.carla.o3.impl.O3Return;
+import br.com.schumaker.carla.o3.impl.O3Variable;
 
 import java.util.List;
 
@@ -13,4 +15,14 @@ public interface VariableTable {
     void addParameter(List<O3Parameter> parameters);
 
     List<O3Parameter> getParameters();
+
+    void addVariable(O3Variable variable);
+
+    void addVariables(List<O3Variable> variables);
+
+    List<O3Variable> getVariables();
+
+    void setReturn(O3Return funcReturn);
+
+    O3Return getReturn();
 }
