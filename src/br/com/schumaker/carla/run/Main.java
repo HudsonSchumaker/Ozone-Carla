7e75f79c13d38fd2f2276b562a997c161cbda1aa
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-02 22:44:31 +0200

    changes.

--- src/br/com/schumaker/carla/run/Main.java
+++ src/br/com/schumaker/carla/run/Main.java
@@ -6,7 +6,7 @@ package br.com.schumaker.carla.run;
 public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length > 0) {
-            new Maestro(args).start();
+            new Compiler(args).start();
         } else {
             System.exit(1);
         }
