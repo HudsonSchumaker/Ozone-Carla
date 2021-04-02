955b8c6b462a74a0f9df2b631510a51633c188ce
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-03 00:34:18 +0200

    changes.

--- src/br/com/schumaker/carla/run/Compiler.java
+++ src/br/com/schumaker/carla/run/Compiler.java
@@ -6,7 +6,9 @@ import br.com.schumaker.carla.io.impl.RawSourceFileChecker;
 import br.com.schumaker.carla.io.impl.SourceFileBuilder;
 import br.com.schumaker.carla.io.impl.SourceFileReader;
 import br.com.schumaker.carla.lexer.impl.Lexer;
+import br.com.schumaker.carla.utils.FileUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,19 +20,21 @@ import java.util.List;
 public final class Compiler {
 
     private final List<String> args;
+    private final SourceFileBuilder sourceFileBuilder;
+    private final RawSourceFileChecker rawSourceFileChecker;
 
     public Compiler(String[] args) {
         this.args = Arrays.asList(args);
+        this.sourceFileBuilder = new SourceFileBuilder();
+        this.rawSourceFileChecker = new RawSourceFileChecker();
     }
 
     public void start() throws Exception {
         System.out.println("Reading source folder: " + args.get(0));
-
         var sourceFile = this.initializeSourceFolder(args.get(0));
 
-
         var lexer = new Lexer();
-        var tabula = lexer.createProgram(sourceFile);
+        // var tabula = lexer.createProgram(sourceFile);
         this.machineCode();
     }
 
@@ -39,9 +43,16 @@ public final class Compiler {
 
     }
 
-    public O3File initializeSourceFolder(String path) throws Exception {
-        var file = new SourceFileBuilder().build(path);
-        new RawSourceFileChecker().doCheck(file);
-        return file;
+    public List<O3File> initializeSourceFolder(String path) throws Exception {
+        var paths = FileUtils.getFilePathsFromRoot(path);
+
+        List<O3File> sourceFiles = new ArrayList<>();
+        for (var p : paths) {
+            var o = sourceFileBuilder.build(p);
+            rawSourceFileChecker.doCheck(o);
+            sourceFiles.add(o);
+        }
+
+        return sourceFiles;
     }
 }
