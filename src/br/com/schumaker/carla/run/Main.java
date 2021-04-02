d23aeab4c84f703f18fa0b941cdc5854fb9d0b83
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-02 21:29:52 +0200

    changes

--- src/br/com/schumaker/carla/run/Main.java
+++ src/br/com/schumaker/carla/run/Main.java
@@ -5,11 +5,6 @@ package br.com.schumaker.carla.run;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-
-
-        System.out.println(System.getProperty("user.country"));
-
-
         if (args.length > 0) {
             new Maestro(args).start();
         } else {
