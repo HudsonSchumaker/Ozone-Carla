27f1d8e67b5d802aeeccb68bdefa0611ea2c7cc9
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-03 15:59:12 +0200

    Changes.

--- src/br/com/schumaker/carla/o3/Variable.java
+++ src/br/com/schumaker/carla/o3/Variable.java
@@ -4,7 +4,7 @@ package br.com.schumaker.carla.o3;
  * @author Hudson Schumaker
  */
 public interface Variable extends MemorySpace {
-    boolean isConstant();
 
+    boolean isConstant();
     Object getValue();
 }
