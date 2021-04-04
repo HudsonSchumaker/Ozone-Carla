6ed03893680998717413f203578c07a619b45a49
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-04 17:24:28 +0200

    Changes.

--- src/br/com/schumaker/carla/lexer/ILexer.java
+++ src/br/com/schumaker/carla/lexer/ILexer.java
@@ -3,11 +3,13 @@ package br.com.schumaker.carla.lexer;
 import br.com.schumaker.carla.io.impl.O3File;
 import br.com.schumaker.carla.o3.impl.App;
 
+import java.util.List;
+
 /**
  *
  * @author Hudson Schumaker
  */
 public interface ILexer {
 
-    App createProgram(O3File file);
+    App createProgram(List<O3File> files);
 }
