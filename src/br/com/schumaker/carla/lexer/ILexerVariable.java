8d904ebf04f4ff64e837933978bd7c538636579f
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-04 18:22:46 +0200

    Changes.

--- src/br/com/schumaker/carla/lexer/ILexerVariable.java
+++ src/br/com/schumaker/carla/lexer/ILexerVariable.java
@@ -1,20 +1,10 @@
 package br.com.schumaker.carla.lexer;
 
-import br.com.schumaker.carla.io.impl.O3FileLine;
-import br.com.schumaker.carla.o3.impl.O3Variable;
-
-import java.util.List;
-
 /**
  * @author Hudson Schumaker
  */
 public interface ILexerVariable {
 
-    List<O3Variable> getVariables(String functionName, List<O3FileLine> lines);
 
-    boolean isInitialized(String data);
 
-    String getVariableName(String data);
-
-    String getVariableInternalName(String functionName, String data);
 }
