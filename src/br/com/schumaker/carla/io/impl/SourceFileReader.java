e0d4ea289c396e3c6b9fb77223940270adcf74a5
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-04 18:29:45 +0200

    Changes.

--- src/br/com/schumaker/carla/io/impl/SourceFileReader.java
+++ src/br/com/schumaker/carla/io/impl/SourceFileReader.java
@@ -19,13 +19,6 @@ import java.util.List;
  */
 public class SourceFileReader implements Reader<List<String>> {
 
-    /**
-     * Reads the lines from a .o3 source file. Reads line by line and return in raw.
-     *
-     * @param path Path of the source file.
-     * @return Raw lines
-     * @throws Exception FileNotFound or FileNotSupported.
-     */
     @Override
     public List<String> read(String path) throws Exception {
 
