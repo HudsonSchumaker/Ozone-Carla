49cdd520cb88c16084cdddad0aa096f747835afc
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-04 18:29:55 +0200

    Changes.

--- src/br/com/schumaker/carla/io/Reader.java
+++ src/br/com/schumaker/carla/io/Reader.java
@@ -1,10 +1,17 @@
 package br.com.schumaker.carla.io;
 
 /**
- *
- * @author Hudson Schumaker
  * @param <T>
+ * @author Hudson Schumaker
  */
 public interface Reader<T> {
+
+    /**
+     * Reads the lines from a .o3 source file. Reads line by line and return in raw.
+     *
+     * @param path Path of the source file.
+     * @return Raw lines
+     * @throws Exception FileNotFound or FileNotSupported.
+     */
     T read(String path) throws Exception;
 }
