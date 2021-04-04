e8098641486bbe8f4b5c6ef6954178644fd41ede
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-05 00:10:05 +0200

    Changes.

--- src/br/com/schumaker/carla/o3/Memory.java
+++ src/br/com/schumaker/carla/o3/Memory.java
@@ -3,8 +3,10 @@ package br.com.schumaker.carla.o3;
 /**
  * @author Hudson Schumaker
  */
-public interface MemorySpace {
+public interface Memory {
     String getName();
+
     String getType();
+
     String getInternalName();
 }
