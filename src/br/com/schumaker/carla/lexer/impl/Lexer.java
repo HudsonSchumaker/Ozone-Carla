bece83c8a5f72959d8f26f13b2f192d06de1c974
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-04 17:24:33 +0200

    Changes.

--- src/br/com/schumaker/carla/lexer/impl/Lexer.java
+++ src/br/com/schumaker/carla/lexer/impl/Lexer.java
@@ -4,6 +4,8 @@ import br.com.schumaker.carla.io.impl.O3File;
 import br.com.schumaker.carla.lexer.ILexer;
 import br.com.schumaker.carla.o3.impl.App;
 
+import java.util.List;
+
 /**
  * Main class of O3 (Carla Compiler) Lexer.
  *
@@ -12,7 +14,8 @@ import br.com.schumaker.carla.o3.impl.App;
 public class Lexer implements ILexer {
 
     @Override
-    public App createProgram(O3File file) {
+    public App createProgram(List<O3File> files) {
+
         var lexerFunction = new LexerFunction();
         var functions = lexerFunction.getFunctions(file);
 
