6d96a9c6746045f5c78c2f2fd69e3fc2e52cc4b7
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-05 00:09:41 +0200

    Changes.

--- src/br/com/schumaker/carla/o3/impl/O3Class.java
+++ src/br/com/schumaker/carla/o3/impl/O3Class.java
@@ -1,13 +1,23 @@
 package br.com.schumaker.carla.o3.impl;
 
+import br.com.schumaker.carla.io.impl.O3File;
+import lombok.Data;
+
 import java.util.List;
 
 /**
  * @author Hudson Schumaker
  */
+@Data
 public class O3Class {
 
     private String name;
     private List<O3Variable> variables;
     private List<O3Function> functions;
+    private O3File o3File;
+
+    public O3Class(String name, O3File file) {
+        this.name = name;
+        this.o3File = file;
+    }
 }
